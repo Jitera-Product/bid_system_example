c061c6c333f8f5380053a0a595382a09cdab0605
Carolynn (Jitera) <developers@jitera.com>
2023-11-23 08:32:44 +0000

    feat[database]: update table 'bid_comments' Changes * '/app/models/bid_comment.rb': created * '/app/models/bid_comment.rb': created

--- app/models/bid_comment.rb
+++ app/models/bid_comment.rb
@@ -1,8 +1,10 @@
 class BidComment < ApplicationRecord
   # associations
+  belongs_to :bid_item
   belongs_to :bid
   belongs_to :user
   # validations
+  validates :content, presence: true, length: { maximum: 255 }
   validates :comment, presence: true
   validates :comment, length: { in: 1..255 }, if: :comment?
   # timestamps
