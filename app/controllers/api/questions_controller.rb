f36fdc7bbc01711411b404318767b06ab9eabfed
Faustina (Jitera) <developers@jitera.com>
2023-11-30 13:08:27 +0000

    feat[api]: implement 'Moderate Content' Changes * '/app/controllers/api/questions_controller.rb': + In the QuestionsController, create a new method called 'update' which will handle the PUT request to the endpoint '/api/questions/{id}'..+ This method should first authenticate the user and check if the user is an administrator. If not, return a 403 Forbidden response..+ Next, validate the paramet...

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,11 +1,22 @@
 class Api::QuestionsController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: %i[create]
+  before_action :doorkeeper_authorize!, only: %i[create update]
   def create
     user = User.find_by(id: params[:user_id])
     return render json: { error: 'User not found or not a contributor' }, status: :unprocessable_entity unless user&.role == 'contributor'
     question = Question.new(question_params.merge(user_id: user.id))
     if question.save
       render json: { message: 'Question successfully created', question_id: question.id }, status: :created
+    else
+      render json: { error: question.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+  def update
+    user = User.find_by(id: params[:user_id])
+    return render json: { error: 'User not found or not an admin' }, status: :forbidden unless user&.role == 'admin'
+    question = Question.find_by(id: params[:id])
+    return render json: { error: 'This question is not found' }, status: :bad_request unless question
+    if question.update(question_params.merge(user_id: user.id))
+      render json: { message: 'Question successfully updated', question: question }, status: :ok
     else
       render json: { error: question.errors.full_messages }, status: :unprocessable_entity
     end
