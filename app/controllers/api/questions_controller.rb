c5c7c44c2ba7db1732b8c2b5ca64fcfa97d6c626
Katharine (Jitera) <developers@jitera.com>
2023-11-30 04:42:12 +0000

    feat[api]: implement 'Submit Question' Changes * '/app/controllers/api/questions_controller.rb': + Create a new method named `create` in the QuestionsController. This method will handle the POST request to submit a question..+ Use strong parameters to permit the `content` and `user_id` parameters from the request body..+ Validate the parameters. If the `content` is blank, return a 400 Bad Reque...

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -2,7 +2,16 @@ class Api::QuestionsController < Api::BaseController
   before_action :doorkeeper_authorize!, only: %i[index create show update moderate]
   before_action :find_question_or_answer, only: [:moderate]
   before_action :authorize_admin, only: [:moderate]
+  before_action :authorize_contributor, only: [:create]
   # other methods...
+  def create
+    question = Question.new(question_params)
+    if question.save
+      render json: { status: 200, question: question }, status: :ok
+    else
+      render json: { message: 'Internal Server Error' }, status: :internal_server_error
+    end
+  end
   def moderate
     if @resource.update(moderate_params)
       render json: { message: 'Content has been moderated successfully.', id: @resource.id }, status: :ok
@@ -12,6 +21,12 @@ class Api::QuestionsController < Api::BaseController
     end
   end
   private
+  def question_params
+    params.require(:question).permit(:content, :user_id).tap do |whitelisted|
+      raise 'The question content is required.' if whitelisted[:content].blank?
+      raise 'Wrong format.' unless whitelisted[:user_id].is_a?(Integer)
+    end
+  end
   def moderate_params
     params.require(:moderate).permit(:content)
   end
@@ -23,5 +38,10 @@ class Api::QuestionsController < Api::BaseController
     user = User.find(params[:user_id])
     raise ActiveRecord::RecordNotFound if user.blank?
     raise 'Unauthorized' unless user.admin?
+  end
+  def authorize_contributor
+    user = User.find(params[:user_id])
+    raise ActiveRecord::RecordNotFound if user.blank?
+    raise 'Forbidden' unless user.contributor?
   end
 end
