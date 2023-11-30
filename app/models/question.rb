1c40ff126edb692d276d6163011ba92fdd0bd50a
Krystle (Jitera) <developers@jitera.com>
2023-11-30 04:41:02 +0000

    feat[database]: update table 'questions' Changes * '/db/migrate/20231122185134_update_questions_table.rb': created * '/app/models/question.rb': created

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :user
   has_many :answers, dependent: :destroy
-  validates :content, :feedback_type, presence: true
+  validates :content, :feedback_type, presence: true, length: { maximum: 255 }
 end
--- db/migrate/20231122185134_update_questions_table.rb
+++ db/migrate/20231122185134_update_questions_table.rb
@@ -1,6 +1,9 @@
 class UpdateQuestionsTable < ActiveRecord::Migration[6.0]
   def change
-    add_reference :questions, :user, foreign_key: true
-    add_column :questions, :feedback_type, :string, null: false
+    change_table :questions do |t|
+      t.change :content, :string, limit: 255, null: false
+      t.references :user, foreign_key: true
+      t.column :feedback_type, :string, null: false
+    end
   end
 end
