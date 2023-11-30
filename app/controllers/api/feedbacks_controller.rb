69e96c111272b2e3e680f975bba9b3ab7ecb2551
Nonie (Jitera) <developers@jitera.com>
2023-11-30 06:10:49 +0000

    feat[api]: implement 'Provide Feedback' Changes * '/app/controllers/api/feedbacks_controller.rb': + Create a new method `create` in the `FeedbacksController` class. This method will handle the POST request to the `/api/feedback` endpoint..+ The `create` method should accept three parameters: `content`, `inquirer_id`, and `answer_id`. These parameters should be extracted from the request body..+ ...

--- app/controllers/api/feedbacks_controller.rb
+++ app/controllers/api/feedbacks_controller.rb
@@ -1,9 +1,10 @@
 class Api::FeedbacksController < Api::BaseController
   before_action :doorkeeper_authorize!, only: %i[create]
+  before_action :authorize_inquirer!, only: %i[create]
   def create
     feedback_service = FeedbackService.new(feedback_params, current_resource_owner)
     if feedback_service.create_feedback
-      render json: { id: feedback_service.feedback.id, message: 'Feedback successfully created' }, status: :created
+      render json: { status: 200, feedback: feedback_service.feedback }, status: :ok
     else
       render json: { error: feedback_service.errors.full_messages }, status: :unprocessable_entity
     end
@@ -11,5 +12,8 @@ class Api::FeedbacksController < Api::BaseController
   private
   def feedback_params
     params.require(:feedback).permit(:content, :inquirer_id, :answer_id)
+  end
+  def authorize_inquirer!
+    render json: { error: 'Forbidden' }, status: :forbidden unless current_resource_owner.role == 'inquirer'
   end
 end
