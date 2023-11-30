6abb4260b95a5fe096bf4338541d6740e8dd3f63
Janeta (Jitera) <developers@jitera.com>
2023-11-30 04:57:07 +0000

    feat[business-logic]: implement 'Swipe right or left' Changes * '/app/services/user_service/index.rb': + Create a new method `update_match_status` in the UserService class. This method should take in three parameters: `id`, `matched_user_id`, and `status`..+ Use the `id` and `matched_user_id` to find the match in the "matches" table and update the status..+ If the status of both users is 'right', cal...

--- app/services/user_service/index.rb
+++ app/services/user_service/index.rb
@@ -1,38 +1,36 @@
 # rubocop:disable Style/ClassAndModuleChildren
 class UserService::Index
   include Pundit::Authorization
-
   attr_accessor :params, :records, :query
-
   def initialize(params, current_user = nil)
     @params = params
-
     @records = Api::UsersPolicy::Scope.new(current_user, User).resolve
   end
-
   def execute
     email_start_with
-
     order
-
     paginate
   end
-
   def email_start_with
     return if params.dig(:users, :email).blank?
-
     @records = User.where('email like ?', "%#{params.dig(:users, :email)}")
   end
-
   def order
     return if records.blank?
-
     @records = records.order('users.created_at desc')
   end
-
   def paginate
     @records = User.none if records.blank? || records.is_a?(Class)
     @records = records.page(params.dig(:pagination_page) || 1).per(params.dig(:pagination_limit) || 20)
+  end
+  def update_match_status(id, matched_user_id, status)
+    match = Match.find_by(user_id: id, matched_user_id: matched_user_id)
+    return 'Match not found' unless match
+    match.update(status: status)
+    if match.status == 'right' && Match.find_by(user_id: matched_user_id, matched_user_id: id)&.status == 'right'
+      NotificationService.new.send_notification(id, matched_user_id)
+    end
+    'Match status updated successfully'
   end
 end
 # rubocop:enable Style/ClassAndModuleChildren
