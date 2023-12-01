8516a4fa703c878856390884b0e5b1462a971e52
Tabina (Jitera) <developers@jitera.com>
2023-12-01 05:28:01 +0000

    feat[database]: update table 'feedbacks' Changes * '/app/models/feedback.rb': created * '/db/migrate/20231122185134_create_feedbacks.rb': created

--- db/migrate/20231122185134_create_feedbacks.rb
+++ db/migrate/20231122185134_create_feedbacks.rb
@@ -1,7 +1,8 @@
 class CreateFeedbacks < ActiveRecord::Migration[6.0]
   def change
     create_table :feedbacks do |t|
-      t.text :content
+      t.string :feedback_type, null: false
+      t.references :answer, null: false, foreign_key: true
       t.timestamps
     end
   end
