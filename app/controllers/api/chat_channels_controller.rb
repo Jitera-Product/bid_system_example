e23ff29b26495d209da1ee09d95ae784a607661d
Merry (Jitera) <developers@jitera.com>
2023-12-01 07:40:02 +0000

    feat[business-logic]: implement 'Close chat channel' Changes * '/app/controllers/api/chat_channels_controller.rb': + Create a new method `close_chat_channel` in the `ChatChannelsController` class. This method will handle the business logic of closing a chat channel..+ The method should take in a parameter `chat_channel_id`..+ Use ActiveRecord's `find_by` method to find the chat channel with the given `chat_chann...

---
+++ app/controllers/api/chat_channels_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  class ChatChannelsController < BaseController
+    before_action :authenticate_user!
+    def close_chat_channel
+      chat_channel_id = params[:id]
+      chat_channel = ChatChannel.find_by(id: chat_channel_id)
+      if chat_channel.nil?
+        render json: { message: 'Chat channel not found' }, status: :not_found
+        return
+      end
+      if chat_channel.is_closed
+        render json: { message: 'Chat channel is already closed' }, status: :unprocessable_entity
+        return
+      end
+      begin
+        chat_channel.update!(is_closed: true)
+        render json: { message: 'Chat channel closed successfully' }, status: :ok
+      rescue => e
+        render json: { message: e.message }, status: :internal_server_error
+      end
+    end
+  end
+end
