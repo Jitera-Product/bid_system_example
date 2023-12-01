0fd110ac7a84b5c5e1079635d0879bb6ca6a3659
Ilysa (Jitera) <developers@jitera.com>
2023-12-01 05:24:25 +0000

    feat[business-logic]: implement 'Retrieve answer' Changes * '/app/controllers/api/answers_controller.rb': + Create a new method called 'retrieve_answer' in the AnswersController. This method will take in a parameter 'query'..+ Use the 'query' parameter to search the 'questions' and 'answers' tables in the database. You can use ActiveRecord's 'where' method to perform the search. For example, you can use... * '/app/controllers/api/questions_controller.rb': + Create a new method named 'retrieve_answer' in the QuestionsController. This method will take 'query' as an input parameter..+ Use natural language processing (NLP) library to understand the context of the query. You can use gems like 'treat' or 'stanford-core-nlp' for NLP in Ruby..+ Search the 'q...

--- app/controllers/api/answers_controller.rb
+++ app/controllers/api/answers_controller.rb
@@ -1,5 +1,21 @@
-class Api::AnswersController < Api::BaseController
+class Api::AnswersController < ApplicationController
   before_action :doorkeeper_authorize!, only: %i[update]
+  def retrieve_answer
+    query = params[:query]
+    questions = Question.where("content LIKE ?", "%#{query}%")
+    answers = Answer.where("content LIKE ?", "%#{query}%")
+    # Combine questions and answers into a single array
+    results = questions + answers
+    # Sort results based on relevance
+    sorted_results = results.sort_by { |result| result.content.downcase.include?(query.downcase) ? -1 : 1 }
+    # Return the most relevant answer
+    most_relevant_answer = sorted_results.first
+    if most_relevant_answer.present?
+      render json: {answer: most_relevant_answer.content, question_id: most_relevant_answer.question_id, answer_id: most_relevant_answer.id}
+    else
+      render json: {message: "No relevant answer found"}, status: :not_found
+    end
+  end
   def update
     @answer = Answer.find_by(id: params[:id], user_id: params[:user_id])
     if @answer.nil?
--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,35 +1,67 @@
-class Api::QuestionsController < Api::BaseController
+class Api::QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
   before_action :doorkeeper_authorize!, only: %i[index create show update]
   def index
     @questions = QuestionService::Index.new(params.permit!, current_resource_owner).execute
     @total_pages = @questions.total_pages
   end
   def show
-    @question = Question.find_by!('questions.id = ?', params[:id])
     authorize @question, policy_class: Api::QuestionsPolicy
+  end
+  def new
+    @question = Question.new
+  end
+  def edit
   end
   def create
-    @question = Question.new(create_params)
+    @question = Question.new(question_params)
     authorize @question, policy_class: Api::QuestionsPolicy
-    return if @question.save
-    @error_object = @question.errors.messages
-    render status: :unprocessable_entity
-  end
-  def create_params
-    params.require(:questions).permit(:content, :user_id)
-  end
-  def update
-    @question = Question.find_by('questions.id = ?', params[:id])
-    raise ActiveRecord::RecordNotFound if @question.blank? || @question.user_id != params[:user_id].to_i
-    authorize @question, policy_class: Api::QuestionsPolicy
-    if @question.update(update_params)
-      render json: { message: 'Question updated successfully', id: @question.id }, status: :ok
-    else
-      @error_object = @question.errors.messages
-      render status: :unprocessable_entity
+    respond_to do |format|
+      if @question.save
+        format.html { redirect_to @question, notice: 'Question was successfully created.' }
+        format.json { render :show, status: :created, location: @question }
+      else
+        format.html { render :new }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
+      end
     end
   end
-  def update_params
-    params.require(:questions).permit(:content)
+  def update
+    authorize @question, policy_class: Api::QuestionsPolicy
+    respond_to do |format|
+      if @question.update(question_params)
+        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
+        format.json { render :show, status: :ok, location: @question }
+      else
+        format.html { render :edit }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  def destroy
+    @question.destroy
+    respond_to do |format|
+      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+  def retrieve_answer
+    query = params[:query]
+    context = query.split(' ').map(&:downcase)
+    relevant_questions = Question.where('content LIKE ?', "%#{context.join(' ')}%")
+    relevant_answers = Answer.where('content LIKE ?', "%#{context.join(' ')}%")
+    if relevant_answers.any?
+      answer = relevant_answers.first
+      render json: {answer: answer.content, question_id: answer.question_id, answer_id: answer.id}
+    else
+      render json: {error: 'No relevant answers found'}, status: :not_found
+    end
+  end
+  private
+  def set_question
+    @question = Question.find(params[:id])
+  end
+  def question_params
+    params.require(:question).permit(:content, :category, :user_id)
   end
 end
