2ec179f20460c3e5bca421d4abd5febf9d13772d
Mira (Jitera) <developers@jitera.com>
2023-12-03 11:06:32 +0000

    feat[business-logic]: implement 'Close chat channel' Changes * '/app/services/chat_channel_service.rb': + Create a method `close_chat_channel` with inputs "chat_channel_id" and "owner_id"..+ Use the `UserService` to validate that "owner_id" corresponds to a logged-in user. This can be done by checking if the user is authenticated and if the "owner_id" matches the current user's ID..+ Check if the "cha...

--- app/services/chat_channel_service.rb
+++ app/services/chat_channel_service.rb
@@ -1,35 +1,28 @@
 class ChatChannelService
-  # Add any other existing methods here
+  include ActiveModel::Model
   def initialize(*_args)
     super
   end
   def create_chat_channel(user_id, bid_item_id)
     begin
-      # Validate that the user_id corresponds to a logged-in user
       unless UserService.logged_in?(user_id)
         raise StandardError.new("User must be logged in to create a chat channel.")
       end
-      # Check if the bid_item_id exists and retrieve the associated owner_id
       bid_item = BidItemService.find_bid_item(bid_item_id)
       raise StandardError.new("Bid item not found.") unless bid_item
-      # Verify that the is_paid status of the BidItem is false
       if bid_item.is_paid
         raise StandardError.new("Cannot create chat channel for a paid bid item.")
       end
-      # Authorize the creation of the chat channel
       unless ChatChannelPolicy.new(user_id, bid_item).authorize_create?
         raise StandardError.new("User is not authorized to create a chat channel for this bid item.")
       end
-      # Create a new ChatChannel
       chat_channel = ChatChannel.new(
         user_id: user_id,
         owner_id: bid_item.user_id,
         bid_item_id: bid_item_id,
         is_active: true
       )
-      # Save the new ChatChannel
       if chat_channel.save
-        # Return the newly created chat channel's ID and is_active status to the user
         return { chat_channel_id: chat_channel.id, is_active: chat_channel.is_active }
       else
         raise StandardError.new("Failed to create chat channel.")
@@ -39,24 +32,20 @@ class ChatChannelService
       raise
     end
   end
-  def close_chat_request(chat_channel_id, owner_id)
-    begin
-      # Validate that the "owner_id" corresponds to a logged-in user.
-      raise StandardError.new('User not logged in') unless UserService.logged_in?(owner_id)
-      # Fetch the ChatChannel using the "chat_channel_id" and check if it exists.
+  def close_chat_channel(chat_channel_id, owner_id)
+    ActiveRecord::Base.transaction do
+      raise StandardError.new('User must be logged in.') unless UserService.logged_in?(owner_id)
       chat_channel = ChatChannel.find_by(id: chat_channel_id)
-      raise StandardError.new('Chat channel not found') unless chat_channel
-      # Check if the logged-in user is the owner of the BidItem associated with the chat channel.
-      raise StandardError.new('User not authorized') unless ChatChannelPolicy.owner?(owner_id, chat_channel.bid_item_id)
-      # Update the "is_active" attribute to false for the given "chat_channel_id".
+      raise StandardError.new('Chat channel does not exist.') unless chat_channel
+      bid_item_owner_id = BidItem.joins(:chat_channels).find_by(chat_channels: { id: chat_channel_id })&.user_id
+      raise StandardError.new('User is not the owner of the bid item.') unless bid_item_owner_id == owner_id
       chat_channel.update!(is_active: false)
-      # Return confirmation that the chat request has been closed.
-      { chat_channel_id: chat_channel.id, is_active: chat_channel.is_active }
-    rescue ActiveRecord::RecordNotFound => e
-      raise StandardError.new(e.message)
+      { chat_channel_id: chat_channel_id, is_active: chat_channel.is_active }
+    rescue ActiveRecord::RecordInvalid => e
+      { error: e.message }
     rescue StandardError => e
-      # Handle exceptions using `StandardError`
       { error: e.message }
     end
   end
+  # Add any other existing methods here
 end
