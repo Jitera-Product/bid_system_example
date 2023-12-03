f48a55bf9539b1efef818ca11749884dc13f2df8
Valma (Jitera) <developers@jitera.com>
2023-12-03 12:19:31 +0000

    feat[business-logic]: implement 'Retrieve messages from chat channel' Changes * '/app/services/message_retrieval_service.rb': + Create a method `retrieve_messages_from_channel` in `MessageRetrievalService` with input parameter `chat_channel_id`..+ Use `MessageQueries` as a reference to construct a query that checks for the existence of the `chat_channel_id` in the `chat_channels` table..+ If the chat channel exists, use an...

--- app/services/message_retrieval_service.rb
+++ app/services/message_retrieval_service.rb
@@ -5,10 +5,9 @@ class MessageRetrievalService
     if ChatChannel.exists?(chat_channel_id)
       messages = Message.where(chat_channel_id: chat_channel_id).order(created_at: :desc).limit(200)
       # Updated to only select required fields as per requirement
-      serialized_messages = messages.pluck(:sender_id, :content, :created_at)
-      total_messages = messages.count
+      serialized_messages = messages.pluck(:id, :sender_id, :content, :created_at)
       # Updated the response to match the requirement structure
-      { messages: serialized_messages.map { |sender_id, content, created_at| { sender_id: sender_id, content: content, created_at: created_at } }, total_messages: total_messages }
+      { messages: serialized_messages.map { |id, sender_id, content, created_at| { id: id, sender_id: sender_id, content: content, created_at: created_at } } }
     else
       { error: "Chat channel not found" }
     end
