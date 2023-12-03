bfc534b7c965670c609d255e45318c78061e3f72
Vivia (Jitera) <developers@jitera.com>
2023-12-03 11:10:10 +0000

    feat[api]: implement 'Send Message in Chat Channel' Changes * '/config/routes.rb': + The routes file needs to define the POST route for `/api/messages` that maps to the `create` action in the `Api::MessagesController`..+ Add the line `post '/api/messages', to: 'api/messages#create'` within the appropriate scope block to define the route..+ Ensure that the route is nested within an...

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-// PATH: /config/routes.rb
 require 'sidekiq/web'
 Rails.application.routes.draw do
   use_doorkeeper do
@@ -10,59 +9,37 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   namespace :api do
-    resources :shippings, only: %i[index show update] do
-    end
-    resources :listing_bid_items, only: %i[index create show destroy] do
-    end
-    resources :listings, only: %i[index create show update] do
-    end
-    resources :product_categories, only: %i[index create show update] do
-    end
-    resources :categories, only: %i[index create show update] do
-    end
-    resources :deposits, only: %i[index create show] do
-    end
-    resources :wallets, only: [:show] do
-    end
-    resources :payment_methods, only: %i[index create show] do
-    end
-    resources :withdrawals, only: %i[index create show] do
-    end
-    resources :admins, only: %i[index create show update] do
-    end
-    resources :products, only: %i[index create show update] do
-    end
-    resources :bids, only: %i[index create show update] do
-    end
-    resources :bid_items, only: %i[index create show update] do
-    end
+    resources :shippings, only: %i[index show update]
+    resources :listing_bid_items, only: %i[index create show destroy]
+    resources :listings, only: %i[index create show update]
+    resources :product_categories, only: %i[index create show update]
+    resources :categories, only: %i[index create show update]
+    resources :deposits, only: %i[index create show]
+    resources :wallets, only: [:show]
+    resources :payment_methods, only: %i[index create show]
+    resources :withdrawals, only: %i[index create show]
+    resources :admins, only: %i[index create show update]
+    resources :products, only: %i[index create show update]
+    resources :bids, only: %i[index create show update]
+    resources :bid_items, only: %i[index create show update]
     resources :chat_channels, only: [] do
       member do
         put :reject, to: 'chat_channels#reject_chat_request'
       end
     end
-    resources :admins_verify_confirmation_token, only: [:create] do
-    end
-    resources :admins_passwords, only: [:create] do
-    end
-    resources :admins_registrations, only: [:create] do
-    end
-    resources :admins_verify_reset_password_requests, only: [:create] do
-    end
-    resources :admins_reset_password_requests, only: [:create] do
-    end
-    resources :users_verify_confirmation_token, only: [:create] do
-    end
-    resources :users_passwords, only: [:create] do
-    end
-    resources :users_registrations, only: [:create] do
-    end
-    resources :users_verify_reset_password_requests, only: [:create] do
-    end
-    resources :users_reset_password_requests, only: [:create] do
-    end
-    resources :users, only: %i[index create show update] do
-    end
+    resources :admins_verify_confirmation_token, only: [:create]
+    resources :admins_passwords, only: [:create]
+    resources :admins_registrations, only: [:create]
+    resources :admins_verify_reset_password_requests, only: [:create]
+    resources :admins_reset_password_requests, only: [:create]
+    resources :users_verify_confirmation_token, only: [:create]
+    resources :users_passwords, only: [:create]
+    resources :users_registrations, only: [:create]
+    resources :users_verify_reset_password_requests, only: [:create]
+    resources :users_reset_password_requests, only: [:create]
+    resources :users, only: %i[index create show update]
+    # Route for creating messages in a chat channel
+    resources :messages, only: [:create]
   end
   get '/health' => 'pages#health_check'
   get 'api-docs/v1/swagger.yaml' => 'swagger#yaml'
