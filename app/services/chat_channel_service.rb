df2abf6f198824997c5018eb725e99519c9b7a05
Yalonda (Jitera) <developers@jitera.com>
2023-12-03 11:05:45 +0000

    feat[business-logic]: implement 'Create chat channel' Changes * '/app/services/chat_channel_service.rb': + Create a method `create_chat_channel` with input parameters `user_id` and `bid_item_id`..+ Use the `UserService` to validate that the `user_id` corresponds to a logged-in user..+ Use the `BidItemService` to check if the `bid_item_id` exists and retrieve the associated `owner_id`..+ Verify that the...

--- app/services/chat_channel_service.rb
+++ app/services/chat_channel_service.rb
@@ -1,23 +1,61 @@
-# /app/services/chat_channel_service.rb
 class ChatChannelService
   # Add any other existing methods here
+  def initialize(*_args)
+    super
+  end
+  def create_chat_channel(user_id, bid_item_id)
+    begin
+      # Validate that the user_id corresponds to a logged-in user
+      unless UserService.logged_in?(user_id)
+        raise StandardError.new("User must be logged in to create a chat channel.")
+      end
+      # Check if the bid_item_id exists and retrieve the associated owner_id
+      bid_item = BidItemService.find_bid_item(bid_item_id)
+      raise StandardError.new("Bid item not found.") unless bid_item
+      # Verify that the is_paid status of the BidItem is false
+      if bid_item.is_paid
+        raise StandardError.new("Cannot create chat channel for a paid bid item.")
+      end
+      # Authorize the creation of the chat channel
+      unless ChatChannelPolicy.new(user_id, bid_item).authorize_create?
+        raise StandardError.new("User is not authorized to create a chat channel for this bid item.")
+      end
+      # Create a new ChatChannel
+      chat_channel = ChatChannel.new(
+        user_id: user_id,
+        owner_id: bid_item.user_id,
+        bid_item_id: bid_item_id,
+        is_active: true
+      )
+      # Save the new ChatChannel
+      if chat_channel.save
+        # Return the newly created chat channel's ID and is_active status to the user
+        return { chat_channel_id: chat_channel.id, is_active: chat_channel.is_active }
+      else
+        raise StandardError.new("Failed to create chat channel.")
+      end
+    rescue StandardError => e
+      logger.error "ChatChannelService#create_chat_channel Error: #{e.message}"
+      raise
+    end
+  end
   def close_chat_request(chat_channel_id, owner_id)
     begin
       # Validate that the "owner_id" corresponds to a logged-in user.
-      raise CustomException.new('User not logged in') unless UserService.logged_in?(owner_id)
+      raise StandardError.new('User not logged in') unless UserService.logged_in?(owner_id)
       # Fetch the ChatChannel using the "chat_channel_id" and check if it exists.
       chat_channel = ChatChannel.find_by(id: chat_channel_id)
-      raise CustomException.new('Chat channel not found') unless chat_channel
+      raise StandardError.new('Chat channel not found') unless chat_channel
       # Check if the logged-in user is the owner of the BidItem associated with the chat channel.
-      raise CustomException.new('User not authorized') unless ChatChannelPolicy.owner?(owner_id, chat_channel.bid_item_id)
+      raise StandardError.new('User not authorized') unless ChatChannelPolicy.owner?(owner_id, chat_channel.bid_item_id)
       # Update the "is_active" attribute to false for the given "chat_channel_id".
       chat_channel.update!(is_active: false)
       # Return confirmation that the chat request has been closed.
       { chat_channel_id: chat_channel.id, is_active: chat_channel.is_active }
     rescue ActiveRecord::RecordNotFound => e
-      raise CustomException.new(e.message)
-    rescue CustomException => e
-      # Handle exceptions using `CustomException`
+      raise StandardError.new(e.message)
+    rescue StandardError => e
+      # Handle exceptions using `StandardError`
       { error: e.message }
     end
   end
