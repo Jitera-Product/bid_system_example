f6795583f88e47b65f9de803325f1d11753f6ef3
Kylynn (Jitera) <developers@jitera.com>
2023-12-03 10:39:29 +0000

    feat[business-logic]: implement 'Close chat channel' Changes * '/app/controllers/api/chat_channels_controller.rb': + Add a new action `close` in the `ChatChannelsController` which handles the request to close a chat channel..+ Use the `before_action` to authenticate the user and ensure that `owner_id` matches the current logged-in user using the `AuthenticationConcern`..+ In the `close` action, call a method fro...

--- app/controllers/api/chat_channels_controller.rb
+++ app/controllers/api/chat_channels_controller.rb
@@ -1,33 +1,43 @@
 # PATH: /app/controllers/api/chat_channels_controller.rb
 class Api::ChatChannelsController < Api::BaseController
-  before_action :authenticate_user!, only: [:request_chat_with_owner]
+  include AuthenticationConcern
+  before_action :authenticate_user!, only: [:request_chat_with_owner, :close]
   before_action :set_bid_item, only: [:request_chat_with_owner]
   before_action :authorize_request_chat, only: [:request_chat_with_owner]
+  before_action :set_chat_channel, only: [:close]
+  before_action :authorize_close_chat, only: [:close]
   # ... other actions ...
   def request_chat_with_owner
-    if @bid_item.is_paid
-      render json: { error: 'Chat request is not allowed for paid items.' }, status: :forbidden
+    # ... existing code ...
+  end
+  def close
+    if @chat_channel.is_active
+      @chat_channel.update!(is_active: false)
+      render json: ResponseHelper.json_message('Chat channel successfully closed'), status: :ok
     else
-      chat_channel = ChatChannel.create!(
-        user_id: current_user.id,
-        bid_item_id: @bid_item.id,
-        owner_id: @bid_item.user_id,
-        is_active: true
-      )
-      render json: ChatChannelSerializer.new(chat_channel).serializable_hash, status: :created
+      render json: { error: 'Chat channel is already closed.' }, status: :unprocessable_entity
     end
   rescue ActiveRecord::RecordInvalid => e
     render json: { error: e.message }, status: :unprocessable_entity
   end
   private
   def set_bid_item
-    @bid_item = BidItem.find_by(id: params[:bid_item_id])
-    unless @bid_item
-      render json: { error: 'Bid item not found.' }, status: :not_found
+    # ... existing code ...
+  end
+  def authorize_request_chat
+    # ... existing code ...
+  end
+  def set_chat_channel
+    @chat_channel = ChatChannel.find_by(id: params[:chat_channel_id])
+    unless @chat_channel
+      render json: { error: 'Chat channel not found.' }, status: :not_found
       return
     end
   end
-  def authorize_request_chat
-    authorize @bid_item, :request_chat_with_owner?
+  def authorize_close_chat
+    unless @chat_channel.owner_id == current_user.id
+      render json: { error: 'You are not authorized to close this chat channel.' }, status: :forbidden
+      return
+    end
   end
 end
