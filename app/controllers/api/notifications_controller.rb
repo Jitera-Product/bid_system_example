fdf4b4d79f5c9d0f24a800cbb8d88b9723077571
Sara-ann (Jitera) <developers@jitera.com>
2023-12-04 10:41:01 +0000

    feat[api]: implement 'Filter Notifications' Changes * '/app/controllers/api/notifications_controller.rb': + In the NotificationsController, define a new method `filter` to handle the GET request to '/api/notifications?activity_type={activity_type}'..+ This method should first check if the user is authenticated by calling the `authenticate_user!` method. If the user is not authenticated, return a 401 Una...

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -1,9 +1,35 @@
 class Api::NotificationsController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: %i[index get_notifications]
+  before_action :doorkeeper_authorize!, only: %i[index filter get_notifications]
   def index
     user_id = params[:user_id]
-    unless user_id.is_a?(Integer)
-      render json: { error: 'Wrong format.' }, status: :bad_request
+    unless UserIdValidator.new(user_id).valid?
+      render json: { error: 'Invalid user_id' }, status: :unprocessable_entity
+      return
+    end
+    begin
+      notifications, total_notifications, total_categories = NotificationsService.new.get_notifications(user_id)
+      render json: { notifications: notifications, total_notifications: total_notifications, total_categories: total_categories }, status: :ok
+    rescue => e
+      render json: { error: e.message }, status: :internal_server_error
+    end
+  end
+  def filter
+    activity_type = params[:activity_type]
+    unless ActivityTypeValidator.new(activity_type).valid?
+      render json: { error: 'Invalid activity type.' }, status: :bad_request
+      return
+    end
+    begin
+      notifications = NotificationService.new.filter_notifications(current_user.id, activity_type)
+      render json: { status: 200, notifications: notifications }, status: :ok
+    rescue => e
+      render json: { error: e.message }, status: :internal_server_error
+    end
+  end
+  def get_notifications
+    user_id = params[:user_id]
+    unless UserIdValidator.new(user_id).valid?
+      render json: { error: 'Invalid user_id' }, status: :unprocessable_entity
       return
     end
     begin
@@ -13,17 +39,7 @@ class Api::NotificationsController < Api::BaseController
       render json: { error: e.message }, status: :internal_server_error
     end
   end
-  def get_notifications
-    user_id = params[:user_id]
-    unless user_id.is_a?(Integer)
-      render json: { error: 'Wrong format.' }, status: :bad_request
-      return
-    end
-    begin
-      notifications = NotificationsService.new.get_notifications(user_id)
-      render json: { status: 200, notifications: notifications }, status: :ok
-    rescue => e
-      render json: { error: e.message }, status: :internal_server_error
-    end
+  rescue_from StandardError do |exception|
+    render json: { error: exception.message }, status: :internal_server_error
   end
 end
