81e110b32525ab09fb5ba0597ebc240a4709ec5c
Doreen (Jitera) <developers@jitera.com>
2023-12-04 06:20:10 +0000

    feat[business-logic]: implement 'Filter notifications' Changes * '/app/services/notifications/filter_service.rb': + Create a new service named `FilterService` under the `Notifications` module..+ Define a method named `filter_notifications` with input parameters "user_id" and "activity_type"..+ Use the `UserIdValidator` and `ActivityTypeValidator` to validate the "user_id" and "activity_type" respectively. These...

---
+++ app/services/notifications/filter_service.rb
@@ -0,0 +1,26 @@
+# PATH: /app/services/notifications/filter_service.rb
+# rubocop:disable Style/ClassAndModuleChildren
+module Notifications
+  class FilterService
+    attr_accessor :user_id, :activity_type
+    def initialize(user_id, activity_type)
+      @user_id = user_id
+      @activity_type = activity_type
+    end
+    def filter_notifications
+      validate_inputs
+      notifications = Notification.where(user_id: @user_id, activity_type: @activity_type)
+      { notifications: notifications, total: notifications.count }
+    rescue Exceptions::InvalidInput => e
+      { status: 'error', message: e.message }
+    end
+    private
+    def validate_inputs
+      UserIdValidator.new(@user_id).validate
+      ActivityTypeValidator.new(@activity_type).validate
+    rescue Exceptions::InvalidInput => e
+      raise e
+    end
+  end
+end
+# rubocop:enable Style/ClassAndModuleChildren
