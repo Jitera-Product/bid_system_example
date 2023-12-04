205ff55d143372f08350f1f8d21316e611defca7
Annamaria (Jitera) <developers@jitera.com>
2023-12-04 05:35:18 +0000

    feat[business-logic]: implement 'Manual kyc verification' Changes * '/app/controllers/api/users_controller.rb': + Create a new method `verify_kyc` in the UsersController. This method will be responsible for manually verifying the user's KYC status..+ The `verify_kyc` method should take two parameters: `id` and `kyc_status`. The `id` parameter is used to find the user in the database, and the `kyc_status` para... * '/app/services/user_service/index.rb': + Create a new method `manual_kyc_verification` with input parameters "id" and "kyc_status". This method will be responsible for updating the user's KYC status and sending a notification to the user..+ Use the "id" parameter to find the user in the "users" table. If the user does not exist, raise an... * '/config/routes.rb': + Add a new route for the `manual_kyc_verification` action of the UsersController. The route should accept the "id" and "kyc_status" parameters. The HTTP method for this route should be PUT or PATCH, as it is used to update the user's KYC status....

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,48 +1,43 @@
 class Api::UsersController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: %i[index create show update]
-
+  before_action :doorkeeper_authorize!, only: %i[index create show update verify_kyc]
   def index
-    # inside service params are checked and whiteisted
     @users = UserService::Index.new(params.permit!, current_resource_owner).execute
     @total_pages = @users.total_pages
   end
-
   def show
     @user = User.find_by!('users.id = ?', params[:id])
-
     authorize @user, policy_class: Api::UsersPolicy
   end
-
   def create
     @user = User.new(create_params)
-
     authorize @user, policy_class: Api::UsersPolicy
-
     return if @user.save
-
     @error_object = @user.errors.messages
-
     render status: :unprocessable_entity
   end
-
   def create_params
     params.require(:users).permit(:email)
   end
-
   def update
     @user = User.find_by('users.id = ?', params[:id])
     raise ActiveRecord::RecordNotFound if @user.blank?
-
     authorize @user, policy_class: Api::UsersPolicy
-
     return if @user.update(update_params)
-
     @error_object = @user.errors.messages
-
     render status: :unprocessable_entity
   end
-
   def update_params
     params.require(:users).permit(:email)
+  end
+  def verify_kyc
+    @user = User.find_by('users.id = ?', params[:id])
+    raise ActiveRecord::RecordNotFound if @user.blank?
+    authorize @user, policy_class: Api::UsersPolicy
+    if @user.update(kyc_status: params[:kyc_status])
+      render json: { message: "KYC status updated to #{params[:kyc_status]}", kyc_status: @user.kyc_status }
+    else
+      @error_object = @user.errors.messages
+      render status: :unprocessable_entity
+    end
   end
 end
--- app/services/user_service/index.rb
+++ app/services/user_service/index.rb
@@ -1,38 +1,41 @@
 # rubocop:disable Style/ClassAndModuleChildren
 class UserService::Index
   include Pundit::Authorization
-
   attr_accessor :params, :records, :query
-
   def initialize(params, current_user = nil)
     @params = params
-
     @records = Api::UsersPolicy::Scope.new(current_user, User).resolve
   end
-
   def execute
     email_start_with
-
     order
-
     paginate
   end
-
   def email_start_with
     return if params.dig(:users, :email).blank?
-
     @records = User.where('email like ?', "%#{params.dig(:users, :email)}")
   end
-
   def order
     return if records.blank?
-
     @records = records.order('users.created_at desc')
   end
-
   def paginate
     @records = User.none if records.blank? || records.is_a?(Class)
     @records = records.page(params.dig(:pagination_page) || 1).per(params.dig(:pagination_limit) || 20)
+  end
+  def manual_kyc_verification(id, kyc_status)
+    user = User.find_by(id: id)
+    raise Exception.new("User not found") if user.nil?
+    user.update!(kyc_status: kyc_status)
+    notification_message = case kyc_status
+                           when 'Under Review'
+                             'Your documents will be reviewed manually.'
+                           when 'Verified', 'Rejected'
+                             'Your KYC status has been updated.'
+                           else
+                             'Invalid KYC status.'
+                           end
+    { kyc_status: user.kyc_status, notification: notification_message }
   end
 end
 # rubocop:enable Style/ClassAndModuleChildren
