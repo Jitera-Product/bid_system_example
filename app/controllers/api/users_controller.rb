29776c25083a95306bc70a5fe85a0cc7b358346d
Auberta (Jitera) <developers@jitera.com>
2023-12-04 05:58:13 +0000

    feat[api]: implement 'KYC Process Timeout' Changes * '/config/routes.rb': + Add a new route for the `restrict` action in UsersController. The route should be a PUT request to the endpoint `/api/users/{user_id}/restrict`.... * '/app/services/user_service/index.rb': + Add a new function `update` in UserService. This function should take `user_id` and `restrict_features` as parameters..+ In the `update` function, first, find the user by `user_id`. If the user doesn't exist, return an error..+ Next, update the `restrict_features` attribute of the user to the valu... * '/app/controllers/api/users_controller.rb': + Add a new action `restrict` in UsersController. This action will handle the PUT request to the endpoint `/api/users/{user_id}/restrict`..+ In the `restrict` action, first, validate the `user_id` parameter. If it's not a number, return a 400 Bad Request with the error message "Wrong format"..+ Next...

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::UsersController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: %i[index create show update update_kyc_status]
+  before_action :doorkeeper_authorize!, only: %i[index create show update update_kyc_status restrict]
   def index
     @users = UserService::Index.new(params.permit!, current_resource_owner).execute
     @total_pages = @users.total_pages
@@ -40,5 +40,17 @@ class Api::UsersController < Api::BaseController
       NotificationService.new(@user).send_kyc_status_update_notification
     end
     render json: { kyc_status: @user.kyc_status, message: 'KYC status updated and notification sent.' }
+  end
+  def restrict
+    user_id = params[:id]
+    unless user_id.to_i.to_s == user_id
+      return render json: { error: 'Wrong format' }, status: :bad_request
+    end
+    begin
+      UserService.update(user_id, restrict_features: true)
+      render json: { status: 200, message: "User's features have been restricted due to incomplete KYC process." }, status: :ok
+    rescue => e
+      render json: { error: e.message }, status: :internal_server_error
+    end
   end
 end
--- app/services/user_service/index.rb
+++ app/services/user_service/index.rb
@@ -1,38 +1,39 @@
+# PATH: /app/services/user_service/index.rb
 # rubocop:disable Style/ClassAndModuleChildren
 class UserService::Index
   include Pundit::Authorization
-
   attr_accessor :params, :records, :query
-
   def initialize(params, current_user = nil)
     @params = params
-
     @records = Api::UsersPolicy::Scope.new(current_user, User).resolve
   end
-
   def execute
     email_start_with
-
     order
-
     paginate
   end
-
   def email_start_with
     return if params.dig(:users, :email).blank?
-
     @records = User.where('email like ?', "%#{params.dig(:users, :email)}")
   end
-
   def order
     return if records.blank?
-
     @records = records.order('users.created_at desc')
   end
-
   def paginate
     @records = User.none if records.blank? || records.is_a?(Class)
     @records = records.page(params.dig(:pagination_page) || 1).per(params.dig(:pagination_limit) || 20)
+  end
+  def update(user_id, restrict_features)
+    return { status: 'error', message: 'Wrong format' } unless user_id.is_a?(Integer)
+    user = User.find_by(id: user_id)
+    return { status: 'error', message: 'User not found' } unless user
+    begin
+      user.update!(restrict_features: restrict_features)
+      { status: 'success', message: "User's features have been restricted due to incomplete KYC process." }
+    rescue StandardError => e
+      { status: 'error', message: e.message }
+    end
   end
 end
 # rubocop:enable Style/ClassAndModuleChildren
