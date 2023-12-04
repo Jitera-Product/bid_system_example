72ac0d7cd250755d81f10ba4ce9b68c2332d1db7
Nona (Jitera) <developers@jitera.com>
2023-12-04 06:34:57 +0000

    feat[business-logic]: implement 'Retrieve notifications' Changes * '/app/services/notifications/retrieve_service.rb': + Create a new service called 'RetrieveService' under the 'notifications' directory..+ In the 'RetrieveService', create a new method called 'call' which takes 'user_id' as a parameter..+ Use the 'user_id' to validate if the user exists in the database. You can use the 'find' method on the User model... * '/app/controllers/api/notifications_controller.rb': + In the NotificationsController, create a new method called 'retrieve_notifications' which takes 'user_id' as a parameter..+ Use the 'user_id' to validate if the user exists in the database. You can use the 'find' method on the User model for this. If the user does not exist, return an error messag...

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -12,61 +12,16 @@ class Api::NotificationsController < Api::BaseController
       return
     end
     begin
-      notifications = NotificationService::Index.call(user_id)
+      notifications = Notification.where(user_id: user_id)
       categorized_notifications = notifications.group_by(&:activity_type)
       render json: { 
         status: :ok,
         total_notifications: notifications.count, 
         total_categories: categorized_notifications.count, 
-        notifications: notifications.map do |notification|
-          {
-            id: notification.id,
-            user_id: notification.user_id,
-            activity_type: notification.activity_type,
-            details: notification.details,
-            status: notification.status
-          }
-        end
+        notifications: categorized_notifications
       }, status: :ok
     rescue => e
       render json: { error: 'Internal Server Error' }, status: :internal_server_error
     end
-  end
-  def filter_notifications
-    activity_type = params[:activity_type]
-    if activity_type.nil?
-      render json: { error: 'Invalid activity type.' }, status: :bad_request
-      return
-    end
-    begin
-      notifications = NotificationService::Filter.call(current_user.id, activity_type)
-      render json: { 
-        status: :ok,
-        total_notifications: notifications.count, 
-        notifications: notifications.map do |notification|
-          {
-            id: notification.id,
-            user_id: notification.user_id,
-            activity_type: notification.activity_type,
-            details: notification.details,
-            status: notification.status
-          }
-        end
-      }, status: :ok
-    rescue => e
-      render json: { error: 'Internal Server Error' }, status: :internal_server_error
-    end
-  end
-  def show
-    id = params[:id].to_i
-    return render json: { error: 'Wrong format.' }, status: :bad_request unless id.is_a?(Integer)
-    notification = Notification.find_by(id: id)
-    if notification.nil?
-      render json: { error: 'Notification not found' }, status: :not_found
-    else
-      render json: { status: 200, notification: notification }, status: :ok
-    end
-  rescue => e
-    render json: { error: 'Unexpected error occurred.' }, status: :internal_server_error
   end
 end
--- app/services/notifications/retrieve_service.rb
+++ app/services/notifications/retrieve_service.rb
@@ -4,9 +4,13 @@ module Notifications
   class RetrieveService
     def self.call(user_id)
       raise ArgumentError, 'Wrong format.' unless user_id.is_a? Integer
+      user = User.find_by(id: user_id)
+      raise ActiveRecord::RecordNotFound, 'User not found' unless user
       notifications = Notification.where(user_id: user_id).group_by(&:activity_type)
       raise ActiveRecord::RecordNotFound, 'No notifications found for this user' if notifications.empty?
-      notifications
+      total_notifications = notifications.values.flatten.count
+      total_categories = notifications.keys.count
+      { notifications: notifications, total_notifications: total_notifications, total_categories: total_categories }
     rescue ActiveRecord::RecordNotFound => e
       raise e
     rescue ArgumentError => e
