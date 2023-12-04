45472a6d01c59e4cf56386f38c722891ed7f6028
Karita (Jitera) <developers@jitera.com>
2023-12-04 18:49:51 +0000

    feat[business-logic]: implement 'Filter notifications by category' Changes * '/app/controllers/api/notifications_controller.rb': + In the NotificationsController, create a new method `filter_by_category` that takes in "user_id" and "activity_type" as parameters..+ Validate the "user_id" and "activity_type" to ensure they exist in the "users" and "notifications" tables respectively. You can use the `find` method for this purpo...

---
+++ app/controllers/api/notifications_controller.rb
@@ -0,0 +1,42 @@
+class Api::NotificationsController < Api::BaseController
+  before_action :doorkeeper_authorize!, only: %i[index create show update filter_by_category]
+  def index
+    @notifications = NotificationService::Index.new(params.permit!, current_resource_owner).execute
+    @total_pages = @notifications.total_pages
+  end
+  def show
+    @notification = Notification.find_by!('notifications.id = ?', params[:id])
+    authorize @notification, policy_class: Api::NotificationsPolicy
+  end
+  def create
+    @notification = Notification.new(create_params)
+    authorize @notification, policy_class: Api::NotificationsPolicy
+    return if @notification.save
+    @error_object = @notification.errors.messages
+    render status: :unprocessable_entity
+  end
+  def create_params
+    params.require(:notifications).permit(:activity_type, :details, :status, :user_id)
+  end
+  def update
+    @notification = Notification.find_by('notifications.id = ?', params[:id])
+    raise ActiveRecord::RecordNotFound if @notification.blank?
+    authorize @notification, policy_class: Api::NotificationsPolicy
+    return if @notification.update(update_params)
+    @error_object = @notification.errors.messages
+    render status: :unprocessable_entity
+  end
+  def update_params
+    params.require(:notifications).permit(:activity_type, :details, :status, :user_id)
+  end
+  def filter_by_category
+    begin
+      user = User.find(params[:user_id])
+      activity_type = params[:activity_type]
+      @notifications = Notification.where(user_id: user.id, activity_type: activity_type)
+      render json: { notifications: @notifications, total: @notifications.count }
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'User or Activity Type not found' }, status: :not_found
+    end
+  end
+end
