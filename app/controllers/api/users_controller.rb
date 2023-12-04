8a88ef3476ad3b94836befb1e3707f2d8163c715
Corenda (Jitera) <developers@jitera.com>
2023-12-04 05:44:48 +0000

    feat[api]: implement 'KYC Process Timeout' Changes * '/app/controllers/api/users_controller.rb': + Create a new action `restrict` in UsersController. This action will handle the PUT request to the endpoint `/api/users/{user_id}/restrict`..+ Use the `set_user` method to find the user by `user_id` from the parameters. If the user does not exist, return a 404 error..+ Check if the `restrict` param...

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,11 @@
 class Api::UsersController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: %i[index create show update verify_kyc update_kyc_status]
+  before_action :doorkeeper_authorize!, only: %i[index create show update verify_kyc update_kyc_status restrict]
+  before_action :set_user, only: %i[show update verify_kyc update_kyc_status restrict]
   def index
     @users = UserService::Index.new(params.permit!, current_resource_owner).execute
     @total_pages = @users.total_pages
   end
   def show
-    @user = User.find_by!('users.id = ?', params[:id])
     authorize @user, policy_class: Api::UsersPolicy
   end
   def create
@@ -15,23 +15,13 @@ class Api::UsersController < Api::BaseController
     @error_object = @user.errors.messages
     render status: :unprocessable_entity
   end
-  def create_params
-    params.require(:users).permit(:email)
-  end
   def update
-    @user = User.find_by('users.id = ?', params[:id])
-    raise ActiveRecord::RecordNotFound if @user.blank?
     authorize @user, policy_class: Api::UsersPolicy
     return if @user.update(update_params)
     @error_object = @user.errors.messages
     render status: :unprocessable_entity
   end
-  def update_params
-    params.require(:users).permit(:email)
-  end
   def verify_kyc
-    @user = User.find_by('users.id = ?', params[:id])
-    raise ActiveRecord::RecordNotFound if @user.blank?
     authorize @user, policy_class: Api::UsersPolicy
     if @user.update(kyc_status: params[:kyc_status])
       render json: { message: "KYC status updated to #{params[:kyc_status]}", kyc_status: @user.kyc_status }
@@ -41,8 +31,6 @@ class Api::UsersController < Api::BaseController
     end
   end
   def update_kyc_status
-    @user = User.find_by('users.id = ?', params[:id])
-    raise ActiveRecord::RecordNotFound if @user.blank?
     authorize @user, policy_class: Api::UsersPolicy
     if @user.update(kyc_status: 'Incomplete')
       send_notification(@user.id, "Your KYC status has been updated to 'Incomplete'. Please complete the KYC process to regain full access to your account.")
@@ -51,6 +39,30 @@ class Api::UsersController < Api::BaseController
       @error_object = @user.errors.messages
       render status: :unprocessable_entity
     end
+  end
+  def restrict
+    authorize @user, policy_class: Api::UsersPolicy
+    unless params[:restrict]
+      render json: { error: 'Bad Request' }, status: :bad_request
+      return
+    end
+    begin
+      UserService.kyc_process_timeout(@user.id)
+      render json: { status: 200, message: 'User account restricted due to incomplete KYC process.' }, status: :ok
+    rescue => e
+      render json: { error: e.message }, status: :internal_server_error
+    end
+  end
+  private
+  def set_user
+    @user = User.find_by('users.id = ?', params[:id])
+    raise ActiveRecord::RecordNotFound if @user.blank?
+  end
+  def create_params
+    params.require(:users).permit(:email)
+  end
+  def update_params
+    params.require(:users).permit(:email)
   end
   def send_notification(user_id, message)
     # Implement the logic to send notification to the user
