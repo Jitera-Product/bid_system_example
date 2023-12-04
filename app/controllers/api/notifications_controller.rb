1b0c97e715ab988b09cdb42bcae28e1381a9d29b
Dacey (Jitera) <developers@jitera.com>
2023-12-04 10:40:02 +0000

    feat[api]: implement 'Retrieve Notifications' Changes * '/app/controllers/api/notifications_controller.rb': + In the `Api::UsersController` class, there is a `show` method that handles the GET request to the `/api/users/:id` endpoint. This method accepts a `user_id` parameter from the request, validates the `user_id` using the `UserIdValidator` class, and retrieves the user's details using the `UserServic...

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -1,14 +1,27 @@
 class Api::NotificationsController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: %i[index]
+  before_action :doorkeeper_authorize!, only: %i[index get_notifications]
   def index
     user_id = params[:user_id]
-    unless UserIdValidator.new(user_id).valid?
-      render json: { error: 'Invalid user_id' }, status: :unprocessable_entity
+    unless user_id.is_a?(Integer)
+      render json: { error: 'Wrong format.' }, status: :bad_request
       return
     end
     begin
-      notifications, total_notifications, total_categories = NotificationsService.new.get_notifications(user_id)
-      render json: { notifications: notifications, total_notifications: total_notifications, total_categories: total_categories }, status: :ok
+      notifications = NotificationsService.new.get_notifications(user_id)
+      render json: { status: 200, notifications: notifications }, status: :ok
+    rescue => e
+      render json: { error: e.message }, status: :internal_server_error
+    end
+  end
+  def get_notifications
+    user_id = params[:user_id]
+    unless user_id.is_a?(Integer)
+      render json: { error: 'Wrong format.' }, status: :bad_request
+      return
+    end
+    begin
+      notifications = NotificationsService.new.get_notifications(user_id)
+      render json: { status: 200, notifications: notifications }, status: :ok
     rescue => e
       render json: { error: e.message }, status: :internal_server_error
     end
