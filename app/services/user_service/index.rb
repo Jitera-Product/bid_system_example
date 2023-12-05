b0da5e64760750e4d819d8b2eee32b70958126ff
Marrissa (Jitera) <developers@jitera.com>
2023-12-05 03:58:59 +0000

    feat[business-logic]: implement 'Social media registration' Changes * '/app/services/user_service/index.rb': + Create a new method `register_social_media` in the `UserService` class. This method should take two parameters: `social_media_platform` and `social_media_id`..+ First, check if the `social_media_id` already exists in the `social_media_accounts` table. You can use ActiveRecord's `exists?` method fo...

--- app/services/user_service/index.rb
+++ app/services/user_service/index.rb
@@ -1,38 +1,33 @@
+# PATH: /app/services/user_service/index.rb
 # rubocop:disable Style/ClassAndModuleChildren
 class UserService::Index
   include Pundit::Authorization
-
   attr_accessor :params, :records, :query
-
   def initialize(params, current_user = nil)
     @params = params
-
     @records = Api::UsersPolicy::Scope.new(current_user, User).resolve
   end
-
   def execute
     email_start_with
-
     order
-
     paginate
   end
-
   def email_start_with
     return if params.dig(:users, :email).blank?
-
     @records = User.where('email like ?', "%#{params.dig(:users, :email)}")
   end
-
   def order
     return if records.blank?
-
     @records = records.order('users.created_at desc')
   end
-
   def paginate
     @records = User.none if records.blank? || records.is_a?(Class)
     @records = records.page(params.dig(:pagination_page) || 1).per(params.dig(:pagination_limit) || 20)
+  end
+  def register_social_media(social_media_platform, social_media_id)
+    return 'Social media account already exists' if SocialMediaAccount.exists?(social_media_id: social_media_id)
+    SocialMediaAccount.create!(social_media_platform: social_media_platform, social_media_id: social_media_id)
+    'Registration successful'
   end
 end
 # rubocop:enable Style/ClassAndModuleChildren
