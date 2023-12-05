2d30773148f45e65486983b574920205a66c3369
Lindsey (Jitera) <developers@jitera.com>
2023-12-05 08:24:23 +0000

    feat[business-logic]: implement 'Lock bid item' Changes * '/app/services/bid_item_lock_service.rb': + Create a method `lock_bid_item` with inputs "user_id: integer" and "bid_item_id: integer"..+ Use the `BidItem` model to find the bid item by "bid_item_id"..+ Check if the current time is before the `expiration_time` of the bid item..+ Confirm that the `user_id` matches the `user_id` associated wit...

--- app/services/bid_item_lock_service.rb
+++ app/services/bid_item_lock_service.rb
@@ -2,28 +2,33 @@
 module Exceptions
   class NotAuthorizedError < StandardError; end
   class InvalidOperationError < StandardError; end
+  class BidItemNotFoundError < StandardError; end
+  class BidItemNotLockableError < StandardError; end
 end
 
 class BidItemLockService
   def lock(user_id, bid_item_id)
-    bid_item = BidItem.find(bid_item_id)
+    bid_item = BidItem.find_by(id: bid_item_id)
+
+    raise Exceptions::BidItemNotFoundError, "Bid item not found." unless bid_item
+    raise Exceptions::BidItemNotLockableError, "Bid item cannot be locked after expiration." if Time.current > bid_item.expiration_time
 
     # The requirement states that we should verify that the "user_id" from the input does not match the "user_id" associated with the bid item.
     # The original code raises an error if the user_id matches, which is incorrect according to the new requirement.
     # Therefore, we need to invert the condition to match the requirement.
     raise Exceptions::NotAuthorizedError, 'User does not have permission to lock this bid item.' unless bid_item.user_id != user_id
 
-    # The rest of the code remains unchanged as it does not conflict with the new requirement.
     lock_bid_item(user_id: user_id, bid_item_id: bid_item_id)
   end
 
   private
 
   def lock_bid_item(user_id:, bid_item_id:)
-    bid_item = BidItem.find(bid_item_id)
+    bid_item = BidItem.find_by(id: bid_item_id)
 
-    # The condition here is also inverted to match the new requirement.
-    raise Exceptions::NotAuthorizedError unless bid_item.user_id != user_id
+    raise Exceptions::BidItemNotFoundError, "Bid item not found." unless bid_item
+    raise Exceptions::BidItemNotLockableError, "Bid item cannot be locked after expiration." if Time.current > bid_item.expiration_time
+    raise Exceptions::NotAuthorizedError unless bid_item.user_id == user_id
 
     if bid_item.status == 'closed'
       raise Exceptions::InvalidOperationError, 'Bid item is already closed and cannot be locked.'
