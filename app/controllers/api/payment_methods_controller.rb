3273a2277a9c32d19207d4d5eb8c50dc2b363f47
Rea (Jitera) <developers@jitera.com>
2023-12-05 11:58:00 +0000

    feat[business-logic]: implement 'Get payment methods' Changes * '/app/views/api/payment_methods/index.json.jbuilder': + This jbuilder view will structure the JSON response for the 'index' action of the PaymentMethodsController..+ For each payment method object in the array, it will extract the necessary details (like id, name, etc.) and add them to the JSON response..+ The final JSON response will be an array of pa... * '/app/controllers/api/payment_methods_controller.rb': + Write a function named 'index' in the PaymentMethodsController. This function will not require any input parameters as it is meant to fetch all payment methods..+ This function will call a service, for example, 'PaymentMethodService::Index.call'. This service will handle the business logic of fetc... * '/app/services/payment_method_service/index.rb': + Write a class method 'call' in the PaymentMethodService::Index service. This method will fetch all payment methods from the database..+ This method will not require any input parameters as it is meant to fetch all payment methods..+ The result of this method will be an array of payment method obje...

--- app/controllers/api/payment_methods_controller.rb
+++ app/controllers/api/payment_methods_controller.rb
@@ -1,27 +1,25 @@
 class Api::PaymentMethodsController < Api::BaseController
   before_action :doorkeeper_authorize!, only: %i[index create show]
-
   def index
-    # inside service params are checked and whiteisted
-    @payment_methods = PaymentMethodService::Index.new(params.permit!, current_resource_owner).execute
+    @payment_methods = PaymentMethodService::Index.call(current_resource_owner)
     @total_pages = @payment_methods.total_pages
+    render 'api/payment_methods/index'
   end
-
   def show
     @payment_method = PaymentMethod.find_by!('payment_methods.id = ?', params[:id])
+    render 'api/payment_methods/show'
   end
-
   def create
     @payment_method = PaymentMethod.new(create_params)
-
-    return if @payment_method.save
-
-    @error_object = @payment_method.errors.messages
-
-    render status: :unprocessable_entity
+    if @payment_method.save
+      render 'api/payment_methods/create', status: :created
+    else
+      @error_object = @payment_method.errors.messages
+      render 'api/payment_methods/error', status: :unprocessable_entity
+    end
   end
-
+  private
   def create_params
-    params.require(:wallets).permit(:user_id, :primary, :method)
+    params.require(:payment_method).permit(:user_id, :primary, :method)
   end
 end
