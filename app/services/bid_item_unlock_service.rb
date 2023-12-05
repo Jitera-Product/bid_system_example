62fb2fddb24d4d06b8fde027c107a1b65b122d72
Cacilia (Jitera) <developers@jitera.com>
2023-12-05 08:24:28 +0000

    feat[business-logic]: implement 'Unlock bid item' Changes * '/app/services/bid_item_unlock_service.rb': + Create a method `unlock_bid_item` with input parameters "user_id: integer, bid_item_id: integer"..+ Use the `BidItem` model to find the bid item by `bid_item_id`..+ Check if the current time is before the `expiration_time` of the bid item..+ Confirm that the `user_id` matches the `user_id` associa...

--- app/services/bid_item_unlock_service.rb
+++ app/services/bid_item_unlock_service.rb
@@ -4,12 +4,17 @@ class BidItemUnlockService
   def unlock_bid_item(user_id:, bid_item_id:)
     bid_item = BidItem.find(bid_item_id)
 
+    raise 'Bid item has expired' if Time.current > bid_item.expiration_time
     raise 'User does not have permission to unlock this bid item' unless bid_item.user_id == user_id
     raise 'Bid item is not locked' unless bid_item.status == 'locked'
 
-    bid_item.status = 'open'
+    bid_item.status = 'active'
     bid_item.save!
 
     "Bid item #{bid_item_id} has been successfully unlocked"
+  rescue ActiveRecord::RecordNotFound
+    "Bid item with id #{bid_item_id} does not exist"
+  rescue => e
+    e.message
   end
 end
