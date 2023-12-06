f2c25b3ce09b3cb2b2f30f48c33ae4f5e45bcdfd
Eddie (Jitera) <developers@jitera.com>
2023-12-06 09:13:31 +0000

    feat[api]: implement 'Send Chat Message' Changes * '/app/controllers/api/chat_messages_controller.rb': + Create a new action `create` in `ChatMessagesController` which inherits from `Api::BaseController`..+ Use `before_action` to authenticate the user and ensure they are authorized to send messages to the channel..+ Validate the `channel_id` by checking if the `ChatChannel` exists using `ChatChannel....

--- app/controllers/api/chat_messages_controller.rb
+++ app/controllers/api/chat_messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::ChatMessagesController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: %i[index]
-  before_action :set_chat_channel, only: %i[index]
+  before_action :doorkeeper_authorize!, only: %i[index create]
+  before_action :set_chat_channel, only: %i[index create]
 
   def index
     if @chat_channel
@@ -14,10 +14,40 @@ class Api::ChatMessagesController < Api::BaseController
     end
   end
 
+  def create
+    if @chat_channel.nil?
+      render json: { error: 'Chat channel not found' }, status: :not_found
+      return
+    end
+
+    message = params[:message]
+    if message.length > 256
+      render json: { error: 'Message exceeds 256 characters limit.' }, status: :unprocessable_entity
+      return
+    end
+
+    if @chat_channel.chat_messages.count >= 500
+      render json: { error: 'Maximum messages per channel is 500.' }, status: :unprocessable_entity
+      return
+    end
+
+    chat_message = @chat_channel.chat_messages.build(message: message, user_id: current_resource_owner.id)
+
+    if chat_message.save
+      render json: { status: 201, chat_message: ChatMessageSerializer.new(chat_message).as_json }, status: :created
+    else
+      render json: { errors: chat_message.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_chat_channel
     @chat_channel = ChatChannel.find_by(id: params[:channel_id])
+  end
+
+  def current_resource_owner
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
 end
 
@@ -28,7 +58,8 @@ class ChatMessageSerializer
 
   def as_json
     {
-      message_id: @message.id,
+      id: @message.id,
+      channel_id: @message.chat_channel_id,
       user_id: @message.user_id,
       message: @message.message,
       created_at: @message.created_at
