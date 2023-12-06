a394998e6116a15cf00e5fd8ce0e57fb99e97b7e
Kassey (Jitera) <developers@jitera.com>
2023-12-06 09:10:25 +0000

    feat[api]: implement 'Fetch Chat Messages' Changes * '/app/controllers/api/chat_channels_controller.rb': + In the `chat_channels_controller.rb`, create a new action `fetch_chat_messages` to handle the GET request for fetching chat messages..+ Use the `before_action` to ensure the user is authenticated and authorized to access the chat channel..+ Validate the `channel_id` parameter to ensure it exists i...

--- app/controllers/api/chat_channels_controller.rb
+++ app/controllers/api/chat_channels_controller.rb
@@ -1,5 +1,6 @@
 class Api::ChatChannelsController < Api::BaseController
   before_action :doorkeeper_authorize!
+  before_action :set_chat_channel, only: [:fetch_chat_messages]
 
   # Existing code here...
 
@@ -23,6 +24,22 @@ class Api::ChatChannelsController < Api::BaseController
     render json: { channel_id: chat_channel.id, created_at: chat_channel.created_at }, status: :created
   rescue ActiveRecord::RecordInvalid => e
     render status: :unprocessable_entity, json: { errors: e.record.errors.full_messages }
+  end
+
+  # New fetch_chat_messages action
+  def fetch_chat_messages
+    if @chat_channel
+      messages = ChatChannelService::Index.fetch_messages(@chat_channel.id)
+      render json: { status: 200, chat_messages: messages }, status: :ok
+    else
+      render status: :not_found, json: { error: 'Chat channel not found.' }
+    end
+  end
+
+  private
+
+  def set_chat_channel
+    @chat_channel = ChatChannel.find_by(id: params[:channel_id])
   end
 
   # Rest of the existing code...
