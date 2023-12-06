be9a1b31e74919803bd2e8a44cf317618c0e7523
Vere (Jitera) <developers@jitera.com>
2023-12-06 23:38:25 +0000

    feat[api]: implement 'Send Message' Changes * '/app/controllers/api/chat_channels_controller.rb': + This file will handle the API endpoint for sending messages within a chat channel..+ The `create` method from `Api::MessagesController` can be used as a reference for handling message creation..+ The `MessageService` class will be used to encapsulate the...

--- app/controllers/api/chat_channels_controller.rb
+++ app/controllers/api/chat_channels_controller.rb
@@ -1,5 +1,6 @@
 class Api::ChatChannelsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_chat_channel, only: [:create]
 
   def create
     bid_item = BidItem.find(params[:bid_item_id])
@@ -17,7 +18,59 @@ class Api::ChatChannelsController < ApplicationController
     render json: { chat_channel_id: chat_channel.id }, status: :created
   end
 
+  def send_message
+    set_chat_channel # Ensure chat_channel is set
+
+    begin
+      validate_message_count
+      validate_content(params[:content])
+      message = @chat_channel.messages.create!(
+        user: current_user,
+        content: params[:content],
+        created_at: Time.current
+      )
+      render json: {
+        status: 201,
+        message: {
+          id: message.id,
+          chat_channel_id: @chat_channel.id,
+          sender_id: current_user.id,
+          content: message.content,
+          created_at: message.created_at
+        }
+      }, status: :created
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+    rescue => e
+      handle_exception(e)
+    end
+  end
+
   private
+
+  def set_chat_channel
+    @chat_channel = ChatChannel.find_by(id: params[:chat_channel_id])
+    render json: { error: 'Chat channel not found' }, status: :not_found unless @chat_channel
+  end
+
+  def validate_message_count
+    if @chat_channel.messages.count >= 500
+      render json: { error: 'Maximum messages per channel is 500.' }, status: :forbidden
+      throw(:abort)
+    end
+  end
+
+  def validate_content(content)
+    if content.length > 256
+      render json: { error: 'Message exceeds 256 characters limit.' }, status: :unprocessable_entity
+      throw(:abort)
+    end
+  end
+
+  def handle_exception(exception)
+    # Assuming there's a method in ApplicationController or included module to handle exceptions
+    render_exception(exception)
+  end
 
   def authenticate_user!
     # Logic to authenticate user by user_id
