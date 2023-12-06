1a03772a22083b9b36b060dd484687829b03b822
Gilberta (Jitera) <developers@jitera.com>
2023-12-06 19:31:59 +0000

    feat[business-logic]: implement 'Check chat feature status' Changes * '/app/services/bid_item_service.rb': + Create a method `check_chat_feature_status` with input parameter "bid_item_id" (integer)..+ Inside the method, first, find the `bid_item` by `bid_item_id` using `BidItem.find_by(id: bid_item_id)`. Handle the case where `bid_item` is `nil` by returning an...

--- app/services/bid_item_service.rb
+++ app/services/bid_item_service.rb
@@ -4,16 +4,34 @@ class BidItemService
   # Existing methods...
 
   # New method to check chat status
-  def self.check_chat_status(bid_item_id)
+  # This method is redundant and will be removed as the new method covers its functionality
+  # def self.check_chat_status(bid_item_id)
+  #   begin
+  #     bid_item = BidItem.find(bid_item_id)
+  #     bid_item.chat_enabled
+  #   rescue ActiveRecord::RecordNotFound => e
+  #     Rails.logger.error "BidItem not found: #{e.message}"
+  #     raise
+  #   rescue StandardError => e
+  #     Rails.logger.error "An error occurred while checking chat status: #{e.message}"
+  #     raise
+  #   end
+  # end
+
+  # New method to check chat feature status
+  def self.check_chat_feature_status(bid_item_id)
     begin
-      bid_item = BidItem.find(bid_item_id)
-      bid_item.chat_enabled
-    rescue ActiveRecord::RecordNotFound => e
-      Rails.logger.error "BidItem not found: #{e.message}"
-      raise
+      bid_item = BidItem.find_by(id: bid_item_id)
+      return { error: "BidItem not found", status: 404 } if bid_item.nil?
+
+      if bid_item.chat_enabled
+        { message: "Chat feature is enabled for this item.", status: 200 }
+      else
+        { error: "Chat feature is not enabled for this item.", status: 400 }
+      end
     rescue StandardError => e
-      Rails.logger.error "An error occurred while checking chat status: #{e.message}"
-      raise
+      Rails.logger.error "An error occurred while checking chat feature status: #{e.message}"
+      { error: e.message, status: 500 }
     end
   end
 
