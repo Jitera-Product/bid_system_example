68c1847bee2f30c1e958e359c2e638ce29be074e
Ula (Jitera) <developers@jitera.com>
2023-12-06 23:32:55 +0000

    feat[database]: update table 'bid_items' and update table 'users' and create table 'chat_channels' and create table 'messages' Changes * '/app/models/message.rb': created * '/db/migrate/20231230123456_create_messages_table.rb': created * '/app/models/chat_channel.rb': created * '/db/migrate/20231230123456_create_chat_channels.rb': created * '/app/models/user.rb': created * '/db/migrate/20230401123456_create_users_table.rb': created * '/db/migrate/20231230123456_create_bid_items_table.rb': created * '/app/models/bid_item.rb': created

--- app/models/bid_item.rb
+++ app/models/bid_item.rb
@@ -3,6 +3,7 @@ class BidItem < ApplicationRecord
            class_name: 'Bid',
            foreign_key: :item_id, dependent: :destroy
   has_many :listing_bid_items, dependent: :destroy
+  has_many :chat_channels, dependent: :destroy # New association
 
   belongs_to :user
   belongs_to :product
@@ -13,15 +14,22 @@ class BidItem < ApplicationRecord
 
   validates :base_price, presence: true
 
-  validates :base_price, numericality: { greater_than_or_equal_to: 0.0, less_than_or_equal_to: 0.0 }
+  # Update the numericality validation for base_price to remove the upper limit
+  validates :base_price, numericality: { greater_than_or_equal_to: 0.0 }
 
   validates :expiration_time, presence: true
 
-  validates :expiration_time, presence: true, timeliness: { type: :datetime, on_or_after: DateTime.tomorrow }
+  # Update the validation for expiration_time to ensure it is in the future
+  validates :expiration_time, presence: true, timeliness: { type: :datetime, on_or_after: -> { DateTime.current } }
 
   validates :name, presence: true
 
-  validates :name, length: { in: 0..255 }, if: :name?
+  # Update the length validation for name to set the correct range
+  validates :name, length: { maximum: 255 }, if: :name?
+
+  # New validations
+  validates :is_chat_enabled, inclusion: { in: [true, false] }
+  validates :status, presence: true
 
   # end for validations
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,32 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :rememberable, :validatable,
-         :trackable, :recoverable, :lockable, :confirmable
+  # Include default devise modules. Others available are:
+  # :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :trackable
 
-  has_one :payment_method, dependent: :destroy
-  has_one :wallet, dependent: :destroy
+  # Associations
+  # The new code has 'has_many :payment_methods' and 'has_many :wallets' which conflicts with the existing 'has_one :payment_method' and 'has_one :wallet'.
+  # To resolve this, we need to decide whether a user should have many or one of each. Assuming a user can have multiple payment methods and wallets, we will use 'has_many'.
+  has_many :payment_methods, dependent: :destroy
+  has_many :wallets, dependent: :destroy
 
   has_many :products, dependent: :destroy
   has_many :bid_items, dependent: :destroy
   has_many :bids, dependent: :destroy
   has_many :deposits, dependent: :destroy
+  has_many :messages, dependent: :destroy
 
-  # validations
-
+  # Validations
+  # The new code does not include password format validation, which is important for security. We will keep the existing password format validation.
   PASSWORD_FORMAT = /\A(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}\z/
   validates :password, format: PASSWORD_FORMAT, if: -> { new_record? || password.present? }
+  validates :email, presence: true, uniqueness: true, length: { in: 0..255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :encrypted_password, presence: true
+  validates :name, presence: true
 
-  validates :email, presence: true, uniqueness: true
-
-  validates :email, length: { in: 0..255 }, if: :email?
-
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-
-  # end for validations
-
+  # Methods
+  # The existing code has a method for generating a reset password token and an authentication method. These should be kept.
   def generate_reset_password_token
     raw, enc = Devise.token_generator.generate(self.class, :reset_password_token)
     self.reset_password_token   = enc
@@ -47,4 +51,6 @@ class User < ApplicationRecord
       false
     end
   end
+
+  # Define any methods that are specific to the User model here
 end
