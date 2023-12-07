d7d9f799e34de90c42030f908ea843b59d66edca
Miranda (Jitera) <developers@jitera.com>
2023-12-07 00:37:20 +0000

    feat[business-logic]: implement 'Check bid item status' Changes * '/app/controllers/api/bid_items_controller.rb': + Implement a new method called `check_bid_item_status` in the `Api::BidItemsController` to encapsulate the logic for checking the status of a bid item..+ This method should accept a parameter `bid_item_id` and fetch the `bid_item` from the database..+ If ...

--- app/controllers/api/bid_items_controller.rb
+++ app/controllers/api/bid_items_controller.rb
@@ -1,8 +1,9 @@
 class Api::BidItemsController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: %i[index create show update]
+  before_action :doorkeeper_authorize!, only: %i[index create show update create_chat_channel check_chat_status]
+  before_action :find_bid_item, only: [:create_chat_channel, :check_chat_status]
+  before_action :check_bid_item_status, only: [:create_chat_channel]
 
   def index
-    # inside service params are checked and whiteisted
     @bid_items = BidItemService::Index.new(params.permit!, current_resource_owner).execute
     @total_pages = @bid_items.total_pages
   end
@@ -38,5 +39,45 @@ class Api::BidItemsController < Api::BaseController
 
   def update_params
     params.require(:bid_items).permit(:user_id, :product_id, :base_price, :status, :name, :expiration_time)
+  end
+
+  def create_chat_channel
+    unless @bid_item.is_chat_enabled
+      render json: { error: 'Can not create a channel for this item.' }, status: :forbidden
+      return
+    end
+
+    begin
+      chat_channel = MessageService.create_chat_channel(@bid_item)
+      render json: { status: 201, chat_channel: chat_channel }, status: :created
+    rescue => e
+      render json: { error: e.message }, status: :internal_server_error
+    end
+  end
+
+  def check_chat_status
+    if @bid_item.nil?
+      render json: { error: 'Bid item not found' }, status: :not_found
+      return
+    end
+
+    if @bid_item.is_chat_enabled
+      render json: { message: 'Chat feature is available for this item' }, status: :ok
+    else
+      render json: { error: 'Chat feature is disabled for this item' }, status: :bad_request
+    end
+  end
+
+  private
+
+  def find_bid_item
+    @bid_item = BidItem.find_by(id: params[:bid_item_id])
+    render json: { error: 'Bid item not found' }, status: :not_found if @bid_item.nil?
+  end
+
+  def check_bid_item_status
+    if @bid_item.status == 'done'
+      render json: { error: 'Bid item already done' }, status: :bad_request
+    end
   end
 end
