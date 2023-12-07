72280d0a5824e2671c1eb9c53f96f733ddbcd3a0
Dodi (Jitera) <developers@jitera.com>
2023-12-07 08:25:05 +0000

    feat[business-logic]: implement 'Enable chat feature for bid item' Changes * '/app/controllers/api/bid_items_controller.rb': + Create a new action `enable_chat` within the `Api::BidItemsController` to handle the enabling of the chat feature for a bid item..+ The action should first authenticate the user and verify that the `current_user.id` matches the `user_id` parameter to ens...

--- app/controllers/api/bid_items_controller.rb
+++ app/controllers/api/bid_items_controller.rb
@@ -1,5 +1,5 @@
 class Api::BidItemsController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: %i[index create show update]
+  before_action :doorkeeper_authorize!, only: %i[index create show update enable_chat]
 
   def index
     # inside service params are checked and whiteisted
@@ -38,5 +38,23 @@ class Api::BidItemsController < Api::BaseController
 
   def update_params
     params.require(:bid_items).permit(:user_id, :product_id, :base_price, :status, :name, :expiration_time)
+  end
+
+  def enable_chat
+    begin
+      bid_item = BidItem.find_by(id: params[:bid_item_id])
+      raise ActiveRecord::RecordNotFound unless bid_item
+
+      if bid_item.user_id == current_resource_owner.id
+        bid_item.update!(chat_enabled: true, updated_at: Time.current)
+        render json: { message: 'Chat feature enabled successfully.' }, status: :ok
+      else
+        render json: { error: 'You are not authorized to enable chat for this bid item.' }, status: :forbidden
+      end
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Bid item not found.' }, status: :not_found
+    rescue => e
+      render json: { error: e.message }, status: :unprocessable_entity
+    end
   end
 end
