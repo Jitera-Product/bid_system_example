05c4d1c1b2933d54910b73913612a6a2bb45e29a
Catlee (Jitera) <developers@jitera.com>
2023-12-07 01:08:57 +0000

    feat[business-logic]: implement 'Check chat feature status' Changes * '/app/controllers/api/bid_items_controller.rb': + Implement a new action `check_chat_status` within the `Api::BidItemsController` to handle the chat feature status check..+ This action should accept a parameter `bid_item_id` and use it to find the corresponding `BidItem`..+ Use `BidItem.find_by!` to fet...

--- app/controllers/api/bid_items_controller.rb
+++ app/controllers/api/bid_items_controller.rb
@@ -1,5 +1,5 @@
 class Api::BidItemsController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: %i[index create show update]
+  before_action :doorkeeper_authorize!, only: %i[index create show update check_chat_status]
 
   def index
     # inside service params are checked and whiteisted
@@ -38,5 +38,17 @@ class Api::BidItemsController < Api::BaseController
 
   def update_params
     params.require(:bid_items).permit(:user_id, :product_id, :base_price, :status, :name, :expiration_time)
+  end
+
+  def check_chat_status
+    bid_item = BidItem.find_by!(id: params[:bid_item_id])
+    
+    if bid_item.is_chat_enabled
+      render json: { is_chat_enabled: true }
+    else
+      render json: { error: "Chat feature is not enabled for this item" }, status: :bad_request
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Bid item not found" }, status: :not_found
   end
 end
