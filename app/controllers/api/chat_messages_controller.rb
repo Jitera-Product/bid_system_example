2ad5e4f6e94d032ce2af09d6c751d984069b51e4
Debee (Jitera) <developers@jitera.com>
2023-12-07 08:24:58 +0000

    feat[business-logic]: implement 'Send chat message' Changes * '/app/controllers/api/chat_messages_controller.rb': This file handles the creation of chat messages and the associated business logic. To implement the requirement, follow these steps:..1. Ensure that the `authenticate_user!` method is correctly verifying that the `user_id` corresponds to a logged-in user. ...

--- app/controllers/api/chat_messages_controller.rb
+++ app/controllers/api/chat_messages_controller.rb
@@ -10,8 +10,8 @@ class Api::ChatMessagesController < ApplicationController
     chat_message = @chat_channel.chat_messages.new(chat_message_params.merge(user_id: current_user.id))
 
     if chat_message.save
-      NotificationService.new.create_notification_for_chat_message(chat_message)
-      render json: { message: 'Chat message sent successfully' }, status: :created
+      NotificationService.new.send_notification_to_bid_item_owner(chat_message)
+      render json: { message: 'Chat message sent successfully', chat_message_id: chat_message.id }, status: :created
     else
       render json: { errors: chat_message.errors.full_messages }, status: :unprocessable_entity
     end
@@ -20,14 +20,22 @@ class Api::ChatMessagesController < ApplicationController
   private
 
   def set_chat_channel
-    @chat_channel = current_user.bid_items.find_by(id: params[:chat_channel_id]).try(:chat_channel)
-    unless @chat_channel
-      render json: { error: 'Chat channel not found or not associated with the current user' }, status: :not_found
+    @chat_channel = ChatChannel.find_by(id: params[:chat_channel_id])
+    if @chat_channel
+      bid_item = BidItem.find_by(id: @chat_channel.bid_item_id)
+      unless bid_item && bid_item.status == 'active' && bid_item.user_id == current_user.id
+        render json: { error: 'Chat channel not associated with an active bid item for the current user' }, status: :forbidden
+      end
+    else
+      render json: { error: 'Chat channel not found' }, status: :not_found
     end
   end
 
   def validate_message_length
-    if params[:content].length > 256
+    content = params[:content].to_s.strip
+    if content.empty?
+      render json: { error: 'Message content cannot be empty' }, status: :unprocessable_entity
+    elsif content.length > 256
       render json: { error: 'Message content exceeds the allowed length' }, status: :unprocessable_entity
     end
   end
