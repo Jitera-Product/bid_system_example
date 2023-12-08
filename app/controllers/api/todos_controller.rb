f2a7541f4011a8481b48b07589709361377a3fe6
Merry (Jitera) <developers@jitera.com>
2023-12-08 08:46:53 +0000

    feat[api]: implement 'Create Todo Item' Changes * '/config/routes.rb': + In the `routes.rb` file, add a new route that maps the POST request for `/api/todos` to the `create` action in the `todos_controller.rb`..+ The route should be defined within the namespace of `api` to ensure it's treated as an API endpoint..+ Ensure that... * '/app/controllers/api/todos_controller.rb': + In the existing TodosController, add a new action `create` to handle the POST request for creating a new todo item..+ Use strong parameters to permit only the allowed parameters: `title`, `description`, `due_date`, `priority`, `recurring`, `category_id`,...

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,10 +1,41 @@
 module Api
   class TodosController < BaseController
     include FileValidationConcern
-    before_action :doorkeeper_authorize!, only: %i[associate_categories_and_tags]
+    before_action :doorkeeper_authorize!, only: %i[create associate_categories_and_tags]
     before_action :set_todo, only: %i[associate_categories_and_tags attach_files]
 
     # ... other actions ...
+
+    # POST /api/todos
+    def create
+      todo_params = params.permit(:title, :description, :due_date, :priority, :recurring, :category_id, :tag_id, :file_name, :file_content)
+      
+      # Validation for title
+      return render json: { error_message: "The title is required." }, status: :unprocessable_entity if todo_params[:title].blank?
+      
+      # Validation for due_date
+      due_date = DateTime.parse(todo_params[:due_date]) rescue nil
+      return render json: { error_message: "Please provide a valid future due date and time." }, status: :unprocessable_entity if due_date.nil? || due_date < DateTime.now
+      
+      # Validation for priority
+      valid_priorities = ['low', 'medium', 'high'] # Assuming these are the valid priorities
+      return render json: { error_message: "Invalid priority level." }, status: :unprocessable_entity unless valid_priorities.include?(todo_params[:priority])
+      
+      validate_category_and_tag(todo_params[:category_id], todo_params[:tag_id])
+      
+      todo = Todo.create!(todo_params.except(:file_name, :file_content))
+      
+      associate_categories([todo_params[:category_id]]) if todo_params[:category_id].present?
+      associate_tags([todo_params[:tag_id]]) if todo_params[:tag_id].present?
+      
+      attach_files_to_todo(todo, todo_params[:file_name], todo_params[:file_content]) if todo_params[:file_name].present? && todo_params[:file_content].present?
+      
+      render json: { status: 201, todo: todo.as_json(include: :attachments) }, status: :created
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { error_message: e.record.errors.full_messages.join(', ') }, status: :unprocessable_entity
+    rescue => e
+      render json: { error_message: e.message }, status: :internal_server_error
+    end
 
     # POST /api/todos/:todo_id/attachments
     def attach_files
@@ -13,9 +44,20 @@ module Api
       authorize @todo, :update?
 
       attachment_ids = []
-      params[:attachments].each do |file|
-        file_name = file.original_filename
-        file_content = file
+      if params[:attachments].respond_to?(:each)
+        params[:attachments].each do |file|
+          file_name = file.original_filename
+          file_content = file
+
+          return render json: { error_message: "File name is required." }, status: :unprocessable_entity if file_name.blank?
+          return render json: { error_message: "Invalid file format." }, status: :unprocessable_entity unless validate_file(file_content)
+
+          attachment = @todo.attachments.create!(file_name: file_name, file_content: file_content)
+          attachment_ids << attachment.id
+        end
+      else
+        file_name = params[:file_name]
+        file_content = params[:file_content]
 
         return render json: { error_message: "File name is required." }, status: :unprocessable_entity if file_name.blank?
         return render json: { error_message: "Invalid file format." }, status: :unprocessable_entity unless validate_file(file_content)
@@ -52,6 +94,31 @@ module Api
       @todo = Todo.find_by(id: params[:todo_id])
     end
 
-    # ... other private methods ...
+    def associate_categories(category_ids)
+      # ... existing code ...
+    end
+
+    def associate_tags(tag_ids)
+      # ... existing code ...
+    end
+
+    def validate_file(file)
+      # ... existing code ...
+    end
+
+    def validate_category_and_tag(category_id, tag_id)
+      errors = []
+      errors << "Selected category does not exist." unless Category.exists?(category_id)
+      errors << "Selected tag does not exist." unless Tag.exists?(tag_id)
+      render json: { error_message: errors.join(', ') }, status: :unprocessable_entity if errors.any?
+    end
+
+    def attach_files_to_todo(todo, file_name, file_content)
+      if validate_file(file_content)
+        todo.attachments.create!(file_name: file_name, file_content: file_content)
+      else
+        render json: { error_message: "Invalid file format." }, status: :unprocessable_entity
+      end
+    end
   end
 end
