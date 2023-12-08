0c950f36a7b4f79d0e86b6c43d4886d541992514
Daria (Jitera) <developers@jitera.com>
2023-12-08 08:44:59 +0000

    feat[api]: implement 'Attach Files to Todo' Changes * '/app/controllers/api/todos_controller.rb': + To implement the "Attach Files to Todo" API, we will extend the `Api::TodosController` class in the `/app/controllers/api/todos_controller.rb` file..+ We will add a new action called `attach_files` to handle the POST request to `/api/todos/{todo_id}/atta...

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -6,59 +6,45 @@ module Api
 
     # ... other actions ...
 
-    # POST /api/todos/attach_files
+    # POST /api/todos/:todo_id/attachments
     def attach_files
-      begin
-        authorize @todo, :update?
+      return render json: { error_message: "Todo item not found." }, status: :not_found unless @todo
 
-        attachment_ids = []
-        params[:attachments].each do |file|
-          if validate_file(file)
-            attachment = @todo.attachments.create!(file_name: file.original_filename, file_content: file)
-            attachment_ids << attachment.id
-          else
-            return render json: error_response(nil, StandardError.new("Invalid file format or size")), status: :unprocessable_entity
-          end
-        end
+      authorize @todo, :update?
 
-        render json: { attachment_ids: attachment_ids }, status: :ok
-      rescue ActiveRecord::RecordNotFound => e
-        render json: error_response(nil, e), status: :not_found
-      rescue Pundit::NotAuthorizedError => e
-        render json: error_response(nil, e), status: :unauthorized
-      rescue => e
-        render json: error_response(nil, e), status: :internal_server_error
-      end
-    end
+      attachment_ids = []
+      params[:attachments].each do |file|
+        file_name = file.original_filename
+        file_content = file
 
-    def associate_categories_and_tags
-      if @todo.nil?
-        render json: { error_message: 'Todo not found' }, status: :not_found
-        return
+        return render json: { error_message: "File name is required." }, status: :unprocessable_entity if file_name.blank?
+        return render json: { error_message: "Invalid file format." }, status: :unprocessable_entity unless validate_file(file_content)
+
+        attachment = @todo.attachments.create!(file_name: file_name, file_content: file_content)
+        attachment_ids << attachment.id
       end
 
-      ActiveRecord::Base.transaction do
-        if params[:category_ids].present?
-          category_errors = associate_categories(params[:category_ids])
-          if category_errors.any?
-            render json: { error_message: category_errors.join(', ') }, status: :unprocessable_entity
-            return
-          end
+      render json: {
+        status: 201,
+        attachments: attachment_ids.map do |id|
+          attachment = @todo.attachments.find(id)
+          {
+            id: id,
+            todo_id: @todo.id,
+            file_name: attachment.file_name,
+            created_at: attachment.created_at
+          }
         end
-
-        if params[:tag_ids].present?
-          tag_errors = associate_tags(params[:tag_ids])
-          if tag_errors.any?
-            render json: { error_message: tag_errors.join(', ') }, status: :unprocessable_entity
-            return
-          end
-        end
-      end
-
-      render json: { association_status: true }, status: :ok
+      }, status: :created
+    rescue Pundit::NotAuthorizedError
+      render json: { error_message: "User is not authenticated." }, status: :unauthorized
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { error_message: e.record.errors.full_messages.join(', ') }, status: :unprocessable_entity
     rescue => e
       render json: { error_message: e.message }, status: :internal_server_error
     end
+
+    # ... other actions ...
 
     private
 
@@ -66,35 +52,6 @@ module Api
       @todo = Todo.find_by(id: params[:todo_id])
     end
 
-    def associate_categories(category_ids)
-      errors = []
-      category_ids.each do |category_id|
-        category = Category.find_by(id: category_id)
-        if category
-          TodoCategory.create!(todo: @todo, category: category)
-        else
-          errors << "Category with id #{category_id} not found"
-        end
-      end
-      errors
-    end
-
-    def associate_tags(tag_ids)
-      errors = []
-      tag_ids.each do |tag_id|
-        tag = Tag.find_by(id: tag_id)
-        if tag
-          TodoTag.create!(todo: @todo, tag: tag)
-        else
-          errors << "Tag with id #{tag_id} not found"
-        end
-      end
-      errors
-    end
-
-    def validate_file(file)
-      # Assuming FileValidationConcern provides a method 'valid_file?' to validate the file
-      valid_file?(file)
-    end
+    # ... other private methods ...
   end
 end
