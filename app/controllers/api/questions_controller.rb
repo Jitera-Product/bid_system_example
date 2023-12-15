2009d4a24e480cff9f9412c567817aa1b4b9176f
Kimberli (Jitera) <developers@jitera.com>
2023-12-15 06:37:11 +0000

    feat[api]: implement 'Submit Question' Changes * '/app/controllers/api/questions_controller.rb': To implement the "Submit Question" API, you will need to add a new action within the `Api::QuestionsController`. This action will handle the POST request to `/api/questions`. You can use the `create` action in the `users_controller.rb` as an example for st...

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,13 +1,22 @@
 class Api::QuestionsController < Api::BaseController
   include SessionConcern
   include RoleConcern
-  before_action :validate_contributor_session, only: [:update, :create]
-  before_action :validate_contributor_role, only: [:update]
+  before_action :validate_contributor_session, only: [:update, :create, :submit]
+  before_action :validate_contributor_role, only: [:update, :submit]
   before_action :set_question, only: [:update]
 
   # ... other actions ...
 
   def create
+    # Existing create action code...
+  end
+
+  def update
+    # Existing update action code...
+  end
+
+  # New submit action
+  def submit
     # Validate input 'content' using QuestionValidator
     validator = QuestionValidator.new(question_params)
     unless validator.valid?
@@ -17,91 +26,40 @@ class Api::QuestionsController < Api::BaseController
     # Validate 'contributor_id' to ensure it is a Contributor
     contributor = User.find_by(id: question_params[:contributor_id])
     unless contributor&.role == 'Contributor'
-      return render json: { message: 'Invalid contributor ID or contributor does not have the correct role' }, status: :forbidden
+      return render json: { message: 'Invalid contributor ID or contributor not found.' }, status: :unprocessable_entity
     end
 
     # Validate each 'tag_id' in the 'tags' array
     unless Tag.where(id: question_params[:tags]).count == question_params[:tags].size
-      return render json: { message: 'One or more tags are invalid' }, status: :unprocessable_entity
+      return render json: { message: 'One or more tags are invalid.' }, status: :unprocessable_entity
     end
 
-    # Create the question and associate tags using QuestionService::Index
-    question = QuestionService::Index.create_question_with_tags(question_params[:content], contributor, question_params[:tags])
-
-    # Return the 'id' of the newly created question as 'question_id' in the response
-    render json: { question_id: question.id }, status: :created
-  rescue ActiveRecord::RecordInvalid => e
-    render json: { message: e.record.errors.full_messages }, status: :unprocessable_entity
-  end
-
-  def update
-    validate_question_id(params[:id])
-    validate_question_ownership(@question, contributor_id)
-    validate_question_content(params[:content])
-    validate_tags(params[:tags])
-
-    Question.transaction do
-      update_status = QuestionUpdatingService.new(
-        question: @question,
-        content: params[:content],
-        tags: params[:tags]
-      ).execute
-
-      if update_status
-        @question.question_tags.destroy_all
-        params[:tags].each do |tag_id|
-          @question.question_tags.create!(tag_id: tag_id)
-        end
+    # Create the question and associate tags
+    question = Question.new(content: question_params[:content], user_id: question_params[:contributor_id])
+    question.transaction do
+      question.save!
+      question_params[:tags].each do |tag_id|
+        question.tags << Tag.find(tag_id)
       end
     end
 
-    render json: { update_status: update_status }
-  rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotFound => e
-    render json: { error: e.message }, status: :unprocessable_entity
+    # Render the successful response
+    render json: {
+      status: 201,
+      question: {
+        id: question.id,
+        content: question.content,
+        contributor_id: question.user_id,
+        created_at: question.created_at
+      }
+    }, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
   end
 
   private
 
-  def validate_contributor_session
-    # Assuming SessionConcern provides a method to validate session
-    # Also, assuming that the existing code's method to check for a contributor session is correct
-    unless current_user && current_user.role == 'Contributor'
-      render json: { message: 'You must be logged in as a Contributor to perform this action' }, status: :unauthorized
-    end
-  end
-
-  def validate_contributor_role
-    # Assuming RoleConcern provides a method to check for contributor role
-    check_contributor_role
-  end
-
-  def set_question
-    @question = Question.find_by(id: params[:id])
-    raise ActiveRecord::RecordNotFound unless @question
-  end
-
-  def validate_question_id(question_id)
-    QuestionValidator.validate_id(question_id)
-  end
-
-  def validate_question_ownership(question, contributor_id)
-    raise ActiveRecord::RecordInvalid unless question.user_id == contributor_id
-  end
-
-  def validate_question_content(content)
-    QuestionValidator.validate_content(content)
-  end
-
-  def validate_tags(tags)
-    tags.each do |tag_id|
-      TagValidator.validate_id(tag_id)
-    end
-  end
-
-  def contributor_id
-    # Assuming SessionConcern provides a method to get contributor_id from session
-    session_contributor_id
-  end
+  # Existing private methods...
 
   def question_params
     params.require(:question).permit(:content, :contributor_id, tags: [])
