c5bfa716b833743d9611dd0ebbca92812acbc035
Kaleena (Jitera) <developers@jitera.com>
2023-12-15 06:39:51 +0000

    feat[business-logic]: implement 'Provide feedback' Changes * '/app/controllers/api/feedbacks_controller.rb': + Implement the `create` action to handle the feedback submission process according to the provided flow..+ Authenticate the user by checking if they have the 'Inquirer' role before allowing them to submit feedback. This can be done by adding a method `aut...

--- app/controllers/api/feedbacks_controller.rb
+++ app/controllers/api/feedbacks_controller.rb
@@ -2,10 +2,62 @@ class Api::FeedbacksController < ApplicationController
   before_action :authenticate_user!
   before_action :validate_inquirer, only: [:create]
   before_action :validate_answer, only: [:create]
+  before_action :validate_rating, only: [:create], if: -> { feedback_params[:rating].present? }
   before_action :check_existing_feedback, only: [:create]
 
   def create
     feedback = Feedback.new(feedback_params)
+    if feedback_params[:rating].present?
+      handle_rating_feedback(feedback)
+    else
+      handle_usefulness_feedback(feedback)
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:answer_id, :user_id, :inquirer_id, :rating, :usefulness, :comment)
+  end
+
+  def validate_inquirer
+    inquirer_id = params[:feedback][:inquirer_id] || params[:feedback][:user_id]
+    unless current_user.role == 'Inquirer' && User.exists?(id: inquirer_id)
+      render json: { error: 'Invalid inquirer ID or inquirer not found.' }, status: :unauthorized and return
+    end
+  end
+
+  def validate_answer
+    unless Answer.exists?(id: params[:feedback][:answer_id])
+      render json: { error: 'Answer not found' }, status: :not_found and return
+    end
+  end
+
+  def validate_rating
+    rating = params[:feedback][:rating].to_i
+    unless rating.between?(1, 5)
+      render json: { error: 'Invalid rating: must be between 1 and 5' }, status: :unprocessable_entity and return
+    end
+  end
+
+  def check_existing_feedback
+    inquirer_id = params[:feedback][:inquirer_id] || params[:feedback][:user_id]
+    existing_feedback = Feedback.find_by(answer_id: params[:feedback][:answer_id], user_id: inquirer_id)
+    if existing_feedback
+      render json: { error: 'Feedback already exists for this answer by the inquirer' }, status: :conflict and return
+    end
+  end
+
+  def handle_rating_feedback(feedback)
+    if feedback.save
+      update_answer_rating(feedback)
+      render json: { message: 'Feedback successfully recorded', feedback: feedback }, status: :created
+    else
+      render json: { errors: feedback.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def handle_usefulness_feedback(feedback)
     if feedback.usefulness.is_a?(TrueClass) || feedback.usefulness.is_a?(FalseClass)
       if feedback.save
         update_answer_relevance(feedback)
@@ -18,34 +70,13 @@ class Api::FeedbacksController < ApplicationController
     end
   end
 
-  private
-
-  def feedback_params
-    params.require(:feedback).permit(:answer_id, :inquirer_id, :usefulness, :comment)
-  end
-
-  def validate_inquirer
-    unless current_user.role == 'inquirer' && User.exists?(id: params[:feedback][:inquirer_id])
-      render json: { error: 'Invalid inquirer ID or inquirer not found.' }, status: :unauthorized and return
-    end
-  end
-
-  def validate_answer
-    unless Answer.exists?(id: params[:feedback][:answer_id])
-      render json: { error: 'Answer not found.' }, status: :bad_request and return
-    end
-  end
-
-  def check_existing_feedback
-    existing_feedback = Feedback.find_by(answer_id: params[:feedback][:answer_id], inquirer_id: params[:feedback][:inquirer_id])
-    if existing_feedback
-      render json: { error: 'Feedback already exists for this answer by the inquirer' }, status: :conflict and return
-    end
+  def update_answer_rating(feedback)
+    answer = Answer.find(feedback.answer_id)
+    answer.recalculate_rating(feedback.rating)
   end
 
   def update_answer_relevance(feedback)
     answer = Answer.find(feedback.answer_id)
-    # Assuming there is a method in the Answer model to update relevance score
     answer.update_relevance_score(feedback.usefulness)
   end
 end
