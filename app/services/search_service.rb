c731ca76f4da52e8204ea36e8bd1833de4ba6a70
Amalie (Jitera) <developers@jitera.com>
2023-12-15 06:40:21 +0000

    feat[business-logic]: implement 'Retrieve answer' Changes * '/app/services/search_service.rb': + Implement a new method `search_answers_for_query` within the `SearchService` class that takes a single argument `query` which is a string representing the user's search query..+ Utilize natural language processing (NLP) libraries or techniques to parse t...

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -3,23 +3,16 @@ class SearchService
 
   # Implement a method `parse_query` that takes a string input "query" and uses natural language processing (NLP) techniques to extract key terms and intent.
   def parse_query(query)
-    # This is a placeholder for the NLP implementation.
-    # In a real-world scenario, you would integrate with an NLP library or service.
-    # For the purpose of this example, we'll just split the query into terms.
     query.split(/\W+/)
   end
 
   # Create a method `search_questions` that takes the parsed terms as input and searches the 'questions' table for matches.
   def search_questions(terms)
-    # Assuming the terms are an array of keywords.
-    # In a real-world scenario, you might use full-text search capabilities of the database.
     Question.where('content LIKE ?', terms.map { |term| "%#{term}%" })
   end
 
   # Add a method `select_relevant_questions` that takes the search results as input and selects the most relevant question(s).
   def select_relevant_questions(questions)
-    # This is a placeholder for a relevance algorithm.
-    # You might sort by the number of matching terms, the date of the question, or other criteria.
     questions.order(created_at: :desc).limit(5)
   end
 
@@ -29,10 +22,12 @@ class SearchService
   end
 
   # If multiple answers are retrieved, add a method `rank_answers` that applies a relevance algorithm to rank the answers.
-  def rank_answers(answers)
-    # This is a placeholder for an answer ranking algorithm.
-    # You might rank based on the length of the answer, the number of upvotes, or other criteria.
-    answers.sort_by { |answer| answer.content.length }.reverse
+  def rank_answers(answers, terms)
+    answers.sort_by do |answer|
+      term_matches = terms.count { |term| answer.content.include?(term) }
+      feedback_score = answer.feedbacks.average(:rating) || 0
+      term_matches + feedback_score
+    end.reverse
   end
 
   # The main method `get_relevant_answer` should orchestrate the flow described in the requirement.
@@ -41,13 +36,30 @@ class SearchService
     questions = search_questions(terms)
     relevant_questions = select_relevant_questions(questions)
     answers = retrieve_answers(relevant_questions)
-    ranked_answers = rank_answers(answers)
+    ranked_answers = rank_answers(answers, terms)
 
-    # Ensure that the final response includes an array of the most relevant answer(s) and the associated question IDs.
-    # Updated to match the requirement of returning an array of answers and question_ids
     {
       answers: ranked_answers.map(&:content),
       question_ids: ranked_answers.map(&:question_id)
     }
+  end
+
+  # New method `search_answers_for_query` as per the guideline
+  def search_answers_for_query(query)
+    terms = parse_query(query)
+    questions = search_questions(terms)
+    relevant_questions = select_relevant_questions(questions)
+    answers = retrieve_answers(relevant_questions)
+    ranked_answers = rank_answers(answers, terms)
+
+    ranked_answers.map do |answer|
+      {
+        content: answer.content,
+        question: {
+          id: answer.question_id,
+          content: Question.find(answer.question_id).content
+        }
+      }
+    end
   end
 end
