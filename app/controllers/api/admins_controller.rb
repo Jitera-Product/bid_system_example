305eade0a765e475fd19e9ee0c4b0608f0d7f696
Mina (Jitera) <developers@jitera.com>
2023-12-20 06:04:38 +0000

    feat[business-logic]: implement 'List admins' Changes * '/app/services/admin_service/index.rb': + Update the `execute` method to include a structured response that contains the list of admins, total items, and total pages. This will ensure that the response adheres to the requirement of providing a list of admins with pagination details..+ Modify the... * '/app/controllers/api/admins_controller.rb': + Create an `index` action within the `AdminsController` to handle the request for listing admins..+ Use the `AdminService::Index` service to encapsulate the logic for retrieving admin records..+ Ensure the service applies pagination by integrating the `Pa...

--- app/controllers/api/admins_controller.rb
+++ app/controllers/api/admins_controller.rb
@@ -2,9 +2,26 @@ class Api::AdminsController < Api::BaseController
   before_action :doorkeeper_authorize!, only: %i[index create show update]
 
   def index
-    # inside service params are checked and whiteisted
-    @admins = AdminService::Index.new(params.permit!, current_resource_owner).execute
-    @total_pages = @admins.total_pages
+    authorize :admin, policy_class: Api::AdminsPolicy # Ensure only authorized users can access the list of admins
+
+    service = AdminService::Index.new(params.permit!, current_resource_owner)
+    @admins = service.execute
+
+    # Utilize PaginationService for pagination
+    pagination_service = PaginationService.new(@admins, params[:page], params[:per_page])
+    @admins = pagination_service.paginate
+
+    # Format the admin records for the response
+    admin_serializer = AdminSerializer.new(@admins)
+
+    # Include the list of admins, total items, and total pages in the response
+    render json: {
+      admins: admin_serializer.serializable_hash(data: { fields: [:id, :email, :name, :created_at, :updated_at] }),
+      total_items: pagination_service.total_items,
+      total_pages: pagination_service.total_pages
+    }
+  rescue => e
+    render json: { error: e.message }, status: :unprocessable_entity
   end
 
   def show
--- app/services/admin_service/index.rb
+++ app/services/admin_service/index.rb
@@ -2,49 +2,54 @@
 class AdminService::Index
   include Pundit::Authorization
 
-  attr_accessor :params, :records, :query
+  attr_accessor :params, :records, :query, :total_count, :total_pages
 
   def initialize(params, current_user = nil)
     @params = params
-
     @records = Api::AdminsPolicy::Scope.new(current_user, Admin).resolve
   end
 
   def execute
     name_start_with
-
     email_start_with
-
     order
-
     paginate
+
+    {
+      admins: @records.as_json(only: [:id, :email, :name, :created_at, :updated_at]),
+      total_items: @total_count,
+      total_pages: @total_pages
+    }
+  rescue StandardError => e
+    {
+      error: e.message,
+      status: :internal_server_error
+    }
   end
 
   def name_start_with
     return if params.dig(:admins, :name).blank?
 
-    @records = Admin.where('name like ?', "%#{params.dig(:admins, :name)}")
+    @records = @records.where('name LIKE ?', "%#{params.dig(:admins, :name)}%")
   end
 
   def email_start_with
     return if params.dig(:admins, :email).blank?
 
-    @records = if records.is_a?(Class)
-                 Admin.where(value.query)
-               else
-                 records.or(Admin.where('email like ?', "%#{params.dig(:admins, :email)}"))
-               end
+    @records = @records.where('email LIKE ?', "%#{params.dig(:admins, :email)}%")
   end
 
   def order
-    return if records.blank?
-
-    @records = records.order('admins.created_at desc')
+    @records = @records.order(created_at: :desc)
   end
 
   def paginate
-    @records = Admin.none if records.blank? || records.is_a?(Class)
-    @records = records.page(params.dig(:pagination_page) || 1).per(params.dig(:pagination_limit) || 20)
+    page = params.dig(:pagination_page) || 1
+    limit = params.dig(:pagination_limit) || 20
+
+    @records = @records.page(page).per(limit)
+    @total_count = @records.total_count
+    @total_pages = @records.total_pages
   end
 end
 # rubocop:enable Style/ClassAndModuleChildren
