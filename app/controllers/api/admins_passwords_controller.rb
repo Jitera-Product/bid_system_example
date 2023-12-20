e18df0c6868cc50f9b5a95e89bc1f1a71a696c1f
Nevsa (Jitera) <developers@jitera.com>
2023-12-20 06:07:06 +0000

    feat[business-logic]: implement 'Change admin password' Changes * '/app/controllers/api/admins_passwords_controller.rb': + Create an action `update_password` within the `AdminsPasswordsController` to handle the password change request..+ This action should receive the admin "id", "password", and "password_confirmation" from the request parameters..+ Use strong parameters to ...

--- app/controllers/api/admins_passwords_controller.rb
+++ app/controllers/api/admins_passwords_controller.rb
@@ -4,11 +4,37 @@ class Api::AdminsPasswordsController < Api::BaseController
       if current_resource_owner.update(password: params.dig(:new_password))
         head :ok, message: I18n.t('common.200')
       else
-        render json: { messages: current_current_resource_owneruser.errors.full_messages },
+        render json: { messages: current_resource_owner.errors.full_messages },
                status: :unprocessable_entity
       end
     else
       render json: { message: I18n.t('email_login.passwords.old_password_mismatch') }, status: :unprocessable_entity
     end
+  end
+
+  def update_password
+    admin = Admin.find_by(id: update_password_params[:id])
+    if admin.nil?
+      render json: { message: I18n.t('admin.passwords.update.not_found') }, status: :not_found
+      return
+    end
+
+    if update_password_params[:password] == update_password_params[:password_confirmation]
+      if admin.update(password: update_password_params[:password])
+        render json: { message: I18n.t('admin.passwords.update.success') }, status: :ok
+      else
+        render json: { messages: admin.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { message: I18n.t('admin.passwords.update.confirmation_mismatch') }, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { message: I18n.t('admin.passwords.update.not_found') }, status: :not_found
+  end
+
+  private
+
+  def update_password_params
+    params.permit(:id, :password, :password_confirmation)
   end
 end
