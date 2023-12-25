b052c34ecd2cff5e85f570fd92260948be95526d
Janie (Jitera) <developers@jitera.com>
2023-12-25 06:04:27 +0000

    feat[business-logic]: implement 'Moderate content' Changes * '/app/controllers/api/v1/moderate_controller.rb': + Create a new action `moderate_content` within the `ModerateController` to handle the moderation of questions and answers..+ Authenticate the user by adding a `before_action` filter to ensure they have the 'Administrator' role. You can use the `doorkeeper...

--- app/controllers/api/v1/moderate_controller.rb
+++ app/controllers/api/v1/moderate_controller.rb
@@ -4,40 +4,91 @@ module Api
   module V1
     class ModerateController < Api::BaseController
       before_action :doorkeeper_authorize!
-      before_action :set_content, only: [:update]
-      before_action :validate_type, only: [:update]
-      before_action :validate_status, only: [:update]
+      before_action :authenticate_user!
+      before_action :validate_admin_role, only: [:moderate_content]
+      before_action :set_content, only: [:update, :delete_content] # Re-added set_content for update and delete_content actions
 
       def update
-        authorize :moderate, policy_class: Api::ModeratePolicy
+        case params[:action]
+        when 'update'
+          update_content
+        when 'delete'
+          delete_content
+        else
+          render json: { message: "Invalid action." }, status: :bad_request
+        end
+      end
 
-        if @content.update(status: params[:status])
-          render json: { status: 200, message: "Content has been successfully moderated." }, status: :ok
+      def moderate_content
+        case moderate_params[:content_type]
+        when 'question'
+          content = Question.find_by(id: moderate_params[:content_id])
+        when 'answer'
+          content = Answer.find_by(id: moderate_params[:content_id])
+        else
+          render json: { message: "Invalid content type." }, status: :bad_request and return
+        end
+
+        if content.nil?
+          render json: { message: "Content not found." }, status: :not_found and return
+        end
+
+        case moderate_params[:action]
+        when 'approve'
+          content.update(status: 'approved')
+        when 'reject'
+          content.destroy # Assuming rejection means deletion
+        when 'edit'
+          if moderate_params[:edited_content].present?
+            content.update(text: moderate_params[:edited_content])
+          else
+            render json: { message: "Edited content can't be blank." }, status: :unprocessable_entity and return
+          end
+        else
+          render json: { message: "Invalid action." }, status: :bad_request and return
+        end
+
+        render json: { content_id: content.id, action: moderate_params[:action] }, status: :ok
+      end
+
+      private
+
+      def authenticate_user!
+        # Assuming authenticate_user! is defined in Api::BaseController
+      end
+
+      def validate_admin_role
+        # Assuming current_user and admin? methods are defined
+        render json: { message: "Forbidden" }, status: :forbidden unless current_user&.admin?
+      end
+
+      def set_content
+        @content = Question.find_by(id: params[:id]) || Answer.find_by(id: params[:id])
+        render json: { message: "Content not found." }, status: :not_found unless @content
+      end
+
+      def moderate_params
+        params.require(:moderation).permit(:content_id, :content_type, :action, :edited_content)
+      end
+
+      def update_content
+        if @content.update(update_params)
+          render json: { status: 200, message: "Content has been updated successfully." }, status: :ok
         else
           render json: { errors: @content.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
-      private
-
-      def set_content
-        @content = params[:type].classify.constantize.find(params[:id])
-      rescue NameError
-        render json: { message: "Invalid type specified." }, status: :bad_request
-      rescue ActiveRecord::RecordNotFound
-        render json: { message: "Content not found." }, status: :not_found
-      end
-
-      def validate_type
-        unless %w[question answer].include?(params[:type])
-          render json: { message: "Invalid type specified." }, status: :bad_request
+      def delete_content
+        if @content.destroy
+          render json: { status: 200, message: "Content has been successfully deleted." }, status: :ok
+        else
+          render json: { errors: @content.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
-      def validate_status
-        unless %w[approved rejected pending].include?(params[:status])
-          render json: { message: "Invalid status value." }, status: :bad_request
-        end
+      def update_params
+        params.require(:content).permit(:title, :content)
       end
     end
   end
