699fac0b4426758489458f836411bc1a0e102185
Vania (Jitera) <developers@jitera.com>
2023-12-25 05:51:52 +0000

    feat[api]: implement 'Submit Answer' Changes * '/app/controllers/api/answers_controller.rb': + To implement the "Submit Answer" API, you will need to add a new action to the `Api::AnswersController` class. This action will handle the POST request to the `/api/answers` endpoint..+ First, ensure that the user is authenticated and authorized to submi...

--- app/controllers/api/answers_controller.rb
+++ app/controllers/api/answers_controller.rb
@@ -1,8 +1,30 @@
 class Api::AnswersController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: %i[update]
-  before_action :authenticate_contributor!, only: %i[update]
+  before_action :doorkeeper_authorize!, only: %i[create update]
+  before_action :authenticate_contributor!, only: %i[create update]
   before_action :set_answer, only: %i[update]
-  before_action :validate_content_presence, only: %i[update]
+  before_action :validate_content_presence, only: %i[create update]
+  before_action :validate_question_id, only: %i[create]
+
+  def create
+    begin
+      # Instantiate the AnswerSubmissionService with the current user
+      submission_service = AnswerSubmissionService.new(current_user)
+      # Call the submit_answer method with the question_id and content from params
+      answer_id = submission_service.submit_answer(params[:question_id], params[:content], Time.now.utc, Time.now.utc)
+      # Find the newly created answer
+      answer = Answer.find(answer_id)
+      # Render the serialized answer with a 201 status code
+      render json: AnswerSerializer.new(answer).serialized_json, status: :created
+    rescue Pundit::NotAuthorizedError
+      render json: { error: 'User does not have permission to submit an answer.' }, status: :forbidden
+    rescue CustomException::ValidationFailed => e
+      render json: { error: e.message }, status: :unprocessable_entity
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { error: e.message }, status: :unprocessable_entity
+    rescue => e
+      render json: { error: e.message }, status: :internal_server_error
+    end
+  end
 
   def update
     # Assuming AnswerUpdatingService exists and is correctly implemented
@@ -27,23 +49,34 @@ class Api::AnswersController < Api::BaseController
   def authenticate_contributor!
     # Assuming there's a method in ApplicationController that checks user's permissions
     # You might need to implement this method based on your application's authentication logic
-    authorize! :update, @answer
+    action = action_name.to_sym == :create ? :create : :update
+    authorize! action, action == :create ? Answer : @answer
   end
 
   def set_answer
-    @answer = Answer.find_by(id: params[:id])
-    unless @answer && @answer.contributor == current_contributor
+    @answer = Answer.find_by(id: params[:id], question_id: params[:question_id])
+    unless @answer && (action_name.to_sym == :create || @answer.contributor == current_contributor)
       render json: { error: 'Answer not found or permission denied.' }, status: :not_found
     end
   end
 
   def validate_content_presence
-    if params[:answer][:content].blank?
-      render json: { error: 'The content is required.' }, status: :unprocessable_entity
+    content = action_name.to_sym == :create ? params[:content] : params[:answer][:content]
+    render json: { error: 'The content is required.' }, status: :unprocessable_entity if content.blank?
+  end
+
+  def validate_question_id
+    unless Question.exists?(params[:question_id])
+      render json: { error: 'Invalid question ID.' }, status: :unprocessable_entity
     end
   end
 
   def answer_params
-    params.require(:answer).permit(:content)
+    # Allow :updated_at only for the update action
+    if action_name.to_sym == :update
+      params.require(:answer).permit(:content, :updated_at)
+    else
+      params.require(:answer).permit(:content)
+    end
   end
 end
