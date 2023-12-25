664e7d80195fe7b5e074172f996802b3ca1aacbb
Rory (Jitera) <developers@jitera.com>
2023-12-25 05:47:42 +0000

    feat[business-logic]: implement 'Manage user' Changes * '/app/controllers/api/users_controller.rb': + To manage users, create actions within the `UsersController` such as `create`, `update`, and `destroy` to handle user management tasks..+ Implement authentication for the administrator by using a before_action filter that calls a method to check if the c...

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,48 +1,73 @@
 class Api::UsersController < Api::BaseController
   before_action :doorkeeper_authorize!, only: %i[index create show update]
+  before_action :authenticate_admin!, only: %i[create update destroy]
+  before_action :set_user, only: %i[show update destroy]
+  before_action :validate_unique_username, only: %i[create update]
 
   def index
-    # inside service params are checked and whiteisted
     @users = UserService::Index.new(params.permit!, current_resource_owner).execute
     @total_pages = @users.total_pages
+    render json: { users: @users, total_pages: @total_pages }, status: :ok
   end
 
   def show
-    @user = User.find_by!('users.id = ?', params[:id])
-
     authorize @user, policy_class: Api::UsersPolicy
+    render json: @user, status: :ok
   end
 
   def create
-    @user = User.new(create_params)
+    @user = User.new(user_params)
 
     authorize @user, policy_class: Api::UsersPolicy
 
-    return if @user.save
-
-    @error_object = @user.errors.messages
-
-    render status: :unprocessable_entity
-  end
-
-  def create_params
-    params.require(:users).permit(:email)
+    if @user.save
+      UserMailer.confirmation_email(@user).deliver_later
+      render json: { message: 'User created successfully', user: @user }, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
-    @user = User.find_by('users.id = ?', params[:id])
-    raise ActiveRecord::RecordNotFound if @user.blank?
-
     authorize @user, policy_class: Api::UsersPolicy
 
-    return if @user.update(update_params)
-
-    @error_object = @user.errors.messages
-
-    render status: :unprocessable_entity
+    if @user.update(user_params)
+      render json: { message: 'User updated successfully', user: @user }, status: :ok
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
-  def update_params
-    params.require(:users).permit(:email)
+  def destroy
+    authorize @user, policy_class: Api::UsersPolicy
+
+    if @user.destroy
+      render json: { message: 'User deleted successfully' }, status: :ok
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def authenticate_admin!
+    render json: { error: 'Unauthorized' }, status: :unauthorized unless current_resource_owner&.admin?
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+    raise ActiveRecord::RecordNotFound unless @user
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password_hash, :role)
+  end
+
+  def validate_unique_username
+    if User.exists?(username: user_params[:username])
+      render json: { error: 'Username already taken' }, status: :unprocessable_entity
+      return false
+    end
+    true
   end
 end
