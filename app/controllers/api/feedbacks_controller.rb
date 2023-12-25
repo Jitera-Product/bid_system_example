c4df1cb9aad98ba2d5d0d1c5bfa193e17916f853
Tina (Jitera) <developers@jitera.com>
2023-12-25 06:08:13 +0000

    feat[api]: implement 'Provide Feedback' Changes * '/app/controllers/api/feedbacks_controller.rb': + To implement the feedback creation API, you will need to add a new action within the `FeedbacksController` which inherits from `BaseController`..+ The new action will be named `create` and should be defined as a POST method to handle the incoming feedbac...

--- app/controllers/api/feedbacks_controller.rb
+++ app/controllers/api/feedbacks_controller.rb
@@ -5,31 +5,37 @@ module Api
 
     def create
       answer = Answer.find_by(id: feedback_params[:answer_id])
-      inquirer = User.find_by(id: feedback_params[:inquirer_id], role: 'Inquirer')
+      user = User.find_by(id: feedback_params[:user_id])
 
       if answer.nil?
         render json: { error: 'Answer not found.' }, status: :bad_request and return
-      elsif inquirer.nil?
-        render json: { error: 'Inquirer not found.' }, status: :forbidden and return
+      elsif user.nil?
+        render json: { error: 'User not found.' }, status: :bad_request and return
       elsif !feedback_params[:score].to_i.between?(1, 5)
         render json: { error: 'Score must be between 1 and 5.' }, status: :bad_request and return
-      elsif feedback_params[:comment].length > 1000
-        render json: { error: 'You cannot input more than 1000 characters.' }, status: :bad_request and return
       end
 
-      feedback = Feedback.new(feedback_params)
+      feedback_service = FeedbackCreationService.new
+      feedback = feedback_service.create_feedback(
+        feedback_params[:answer_id],
+        feedback_params[:user_id],
+        feedback_params[:score],
+        feedback_params[:comment]
+      )
 
-      if feedback.save
+      if feedback.persisted?
         render json: { status: 201, feedback: feedback.as_json.merge(created_at: feedback.created_at.iso8601) }, status: :created
       else
         render json: error_response(feedback, feedback.errors), status: :unprocessable_entity
       end
+    rescue StandardError => e
+      render json: { error: e.message }, status: :unprocessable_entity
     end
 
     private
 
     def feedback_params
-      params.require(:feedback).permit(:answer_id, :inquirer_id, :score, :comment)
+      params.require(:feedback).permit(:answer_id, :user_id, :score, :comment)
     end
 
     def authenticate_inquirer
