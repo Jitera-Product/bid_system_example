c26eb07935b78309f513637d54844c85bc7f8e2c
Alvinia (Jitera) <developers@jitera.com>
2023-12-26 04:44:50 +0000

    feat[business-logic]: implement 'List Deposits' Changes * '/app/services/deposit_service/index.rb': + Create a method `list_deposits` in the `DepositService::Index` class that accepts `params` as an argument..+ The method should initialize a query with `Deposit.all`..+ If a `value` filter is provided in `params`, apply a scope to filter deposits by value... * '/app/services/deposit_listing_service.rb': + Create a new service class `DepositListingService` that will handle the logic for listing deposits..+ The service should be initialized with a `params` hash that contains filters and pagination options..+ Define private methods for each filter condition ...

--- app/services/deposit_service/index.rb
+++ app/services/deposit_service/index.rb
@@ -4,81 +4,65 @@ class DepositService::Index
 
   def initialize(params, _current_user = nil)
     @params = params
-
     @records = Deposit
   end
 
   def execute
-    value_equal
+    initialize_query
 
-    user_id_equal
+    apply_value_filter if params.dig(:deposits, :value)
+    apply_user_id_filter if params.dig(:deposits, :user_id)
+    apply_wallet_id_filter if params.dig(:deposits, :wallet_id)
+    apply_status_filter if params.dig(:deposits, :status)
+    apply_payment_method_id_filter if params.dig(:deposits, :payment_method_id)
 
-    wallet_id_equal
+    order_query
+    paginate_query
 
-    status_equal
-
-    payment_method_id_equal
-
-    order
-
-    paginate
+    {
+      records: @records,
+      pagination: {
+        current_page: @records.current_page,
+        total_pages: @records.total_pages,
+        total_count: @records.total_count
+      }
+    }
   end
 
-  def value_equal
-    return if params.dig(:deposits, :value).blank?
+  private
 
-    @records = Deposit.where('value = ?', params.dig(:deposits, :value))
+  def initialize_query
+    @records = Deposit.all
   end
 
-  def user_id_equal
-    return if params.dig(:deposits, :user_id).blank?
-
-    @records = if records.is_a?(Class)
-                 Deposit.where(value.query)
-               else
-                 records.or(Deposit.where('user_id = ?', params.dig(:deposits, :user_id)))
-               end
+  def apply_value_filter
+    @records = @records.where('value = ?', params.dig(:deposits, :value))
   end
 
-  def wallet_id_equal
-    return if params.dig(:deposits, :wallet_id).blank?
-
-    @records = if records.is_a?(Class)
-                 Deposit.where(value.query)
-               else
-                 records.or(Deposit.where('wallet_id = ?', params.dig(:deposits, :wallet_id)))
-               end
+  def apply_user_id_filter
+    @records = @records.where('user_id = ?', params.dig(:deposits, :user_id))
   end
 
-  def status_equal
-    return if params.dig(:deposits, :status).blank?
-
-    @records = if records.is_a?(Class)
-                 Deposit.where(value.query)
-               else
-                 records.or(Deposit.where('status = ?', params.dig(:deposits, :status)))
-               end
+  def apply_wallet_id_filter
+    @records = @records.where('wallet_id = ?', params.dig(:deposits, :wallet_id))
   end
 
-  def payment_method_id_equal
-    return if params.dig(:deposits, :payment_method_id).blank?
-
-    @records = if records.is_a?(Class)
-                 Deposit.where(value.query)
-               else
-                 records.or(Deposit.where('payment_method_id = ?', params.dig(:deposits, :payment_method_id)))
-               end
+  def apply_status_filter
+    @records = @records.where('status = ?', params.dig(:deposits, :status))
   end
 
-  def order
-    return if records.blank?
-
-    @records = records.order('deposits.created_at desc')
+  def apply_payment_method_id_filter
+    @records = @records.where('payment_method_id = ?', params.dig(:deposits, :payment_method_id))
   end
 
-  def paginate
-    @records = Deposit.none if records.blank? || records.is_a?(Class)
-    @records = records.page(params.dig(:pagination_page) || 1).per(params.dig(:pagination_limit) || 20)
+  def order_query
+    @records = @records.order('created_at DESC')
+  end
+
+  def paginate_query
+    page = params.dig(:pagination_page) || 1
+    per_page = params.dig(:pagination_limit) || 20
+    @records = @records.page(page).per(per_page)
   end
 end
 # rubocop:enable Style/ClassAndModuleChildren
