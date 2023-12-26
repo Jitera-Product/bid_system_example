ad5874a06d0877202ced747473120f18c412557b
Anthe (Jitera) <developers@jitera.com>
2023-12-26 09:22:52 +0000

    feat[business-logic]: implement 'Provide feedback' Changes * '/app/controllers/api/v1/feedbacks_controller.rb': + Create a new action `create_feedback` within the `FeedbacksController` to handle feedback submissions..+ Utilize the `authenticate_user!` method to ensure the user is authenticated. You can reference the `UsersController` for how authentication is handle... * '/app/services/feedback_creation_service.rb': + Create a service named `FeedbackCreationService` with a method `execute` that takes input parameters: `answer_id`, `user_id`, `score`, and `comment`..+ Authenticate the user by checking if they have the 'Inquirer' role. You can use Pundit for authorizati...

--- app/controllers/api/v1/feedbacks_controller.rb
+++ app/controllers/api/v1/feedbacks_controller.rb
@@ -4,29 +4,30 @@ module Api
     class FeedbacksController < BaseController
       include OauthTokensConcern
 
-      before_action :authenticate_inquirer!, only: [:create_feedback, :provide_feedback]
+      before_action :authenticate_user!, only: [:create_feedback, :provide_feedback]
+      before_action :verify_inquirer_role!, only: [:provide_feedback]
 
       # POST /api/v1/feedbacks/provide_feedback
       def provide_feedback
-        authenticate_inquirer!
+        # Validate input parameters
+        validate_feedback_params
 
+        # Check if the 'answer_id' corresponds to a valid `Answer` record
         answer = Answer.find_by(id: feedback_params[:answer_id])
-        inquirer = User.find_by(id: feedback_params[:inquirer_id])
-
         return render json: { error: 'Answer not found.' }, status: :not_found unless answer
-        return render json: { error: 'Inquirer not found.' }, status: :not_found unless inquirer
-        return render json: { error: 'Score must be between 1 and 5.' }, status: :unprocessable_entity unless feedback_params[:score].to_i.between?(1, 5)
-        return render json: { error: 'You cannot input more than 1000 characters.' }, status: :unprocessable_entity if feedback_params[:comment].length > 1000
 
-        feedback = Feedback.new(
-          answer_id: answer.id,
-          inquirer_id: inquirer.id,
-          score: feedback_params[:score],
-          comment: feedback_params[:comment]
-        )
+        # Check if the 'user_id' corresponds to a valid `User` record
+        user = User.find_by(id: feedback_params[:user_id])
+        return render json: { error: 'User not found.' }, status: :not_found unless user
+
+        # Create a new `Feedback` record with the validated parameters
+        feedback = Feedback.new(feedback_params)
 
         if feedback.save
-          AnswerUpdatingService.new(answer).update_feedback_score
+          # Update the `feedback_score` of the associated `Answer` record
+          AnswerUpdatingService.new(answer).update_feedback_score(feedback.score)
+
+          # Return a confirmation response with the details of the submitted feedback
           render json: { status: 201, feedback: feedback.as_json.merge(created_at: feedback.created_at.iso8601) }, status: :created
         else
           render json: { errors: feedback.errors.full_messages }, status: :unprocessable_entity
@@ -39,16 +40,27 @@ module Api
 
       private
 
-      def authenticate_inquirer!
+      def authenticate_user!
         authenticate_resource_owner!
+      end
+
+      def verify_inquirer_role!
         # Ensure the user has the role of "Inquirer"
         unless current_resource_owner.role == 'Inquirer'
           render json: { error: 'You must be an Inquirer to provide feedback.' }, status: :forbidden
         end
       end
 
+      def validate_feedback_params
+        feedback_params
+        raise ArgumentError, 'Score must be between 1 and 5' unless feedback_params[:score].between?(1, 5)
+        raise ArgumentError, 'Answer ID and User ID must be present' if feedback_params[:answer_id].blank? || feedback_params[:user_id].blank?
+      end
+
       def feedback_params
-        params.require(:feedback).permit(:answer_id, :inquirer_id, :score, :comment)
+        params.require(:feedback).permit(:answer_id, :user_id, :score, :comment).tap do |whitelisted|
+          whitelisted[:score] = whitelisted[:score].to_i
+        end
       end
     end
   end
