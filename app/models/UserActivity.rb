550148e59d56f1b73f622d3d583b534584d29e0f
Laurella (Jitera) <developers@jitera.com>
2023-12-26 09:19:10 +0000

    feat[database]: update table 'user_activities' Changes * '/app/models/UserActivity.rb': created * '/db/migrate/20230409123456_add_action_and_timestamp_to_user_activities.rb': created

--- app/models/UserActivity.rb
+++ app/models/UserActivity.rb
@@ -7,6 +7,8 @@ class UserActivity < ApplicationRecord
   validates :activity_type, presence: true
   validates :activity_description, presence: true
   validates :user_id, presence: true
+  validates :action, presence: true  # Assuming 'action' is a new column to be validated
+  validates :timestamp, presence: true  # Assuming 'timestamp' is a new column to be validated
 
   # end for validations
 
