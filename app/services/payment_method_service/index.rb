5fc624b2062de0ff07a546f5fa128278b4cb0bbd
Jeanette (Jitera) <developers@jitera.com>
2023-12-26 04:44:05 +0000

    feat[business-logic]: implement 'List Payment Methods' Changes * '/app/services/payment_method_service/index.rb': This file contains the business logic for listing payment methods with filters and pagination. To address the requirement, the following guidelines should be followed:.+ Refactor the `initialize` method to accept a second parameter for the current user, wh... * '/app/services/payment_methods/list_service.rb': + Create a class `ListService` within the `PaymentMethods` module that inherits from `BaseService`..+ Define an `initialize` method that accepts `params` and stores them in an instance variable..+ Implement a `call` method that will orchestrate the flow de...

--- app/services/payment_method_service/index.rb
+++ app/services/payment_method_service/index.rb
@@ -1,60 +1,60 @@
 # rubocop:disable Style/ClassAndModuleChildren
 class PaymentMethodService::Index
-  attr_accessor :params, :records, :query
+  attr_accessor :params, :records, :query, :current_user
 
-  def initialize(params, _current_user = nil)
+  def initialize(params, current_user = nil)
     @params = params
-
+    @current_user = current_user
     @records = PaymentMethod
   end
 
   def execute
-    user_id_equal
-
-    primary_equal
-
-    method_equal
-
-    order
-
-    paginate
+    begin
+      user_id_equal
+      primary_equal
+      method_equal
+      order
+      paginate
+    rescue StandardError => e
+      handle_error(e)
+    end
   end
+
+  private
 
   def user_id_equal
     return if params.dig(:wallets, :user_id).blank?
 
-    @records = PaymentMethod.where('user_id = ?', params.dig(:wallets, :user_id))
+    @records = records.where(user_id: params.dig(:wallets, :user_id))
   end
 
   def primary_equal
     return if params.dig(:wallets, :primary).blank?
 
-    @records = if records.is_a?(Class)
-                 PaymentMethod.where(value.query)
-               else
-                 records.or(PaymentMethod.where('primary = ?', params.dig(:wallets, :primary)))
-               end
+    @records = records.where(primary: params.dig(:wallets, :primary))
   end
 
   def method_equal
     return if params.dig(:wallets, :method).blank?
 
-    @records = if records.is_a?(Class)
-                 PaymentMethod.where(value.query)
-               else
-                 records.or(PaymentMethod.where('method = ?', params.dig(:wallets, :method)))
-               end
+    @records = records.where(method: params.dig(:wallets, :method))
   end
 
   def order
-    return if records.blank?
-
-    @records = records.order('payment_methods.created_at desc')
+    @records = records.order(created_at: :desc)
   end
 
   def paginate
-    @records = PaymentMethod.none if records.blank? || records.is_a?(Class)
-    @records = records.page(params.dig(:pagination_page) || 1).per(params.dig(:pagination_limit) || 20)
+    page_number = params.dig(:pagination, :page) || 1
+    per_page = params.dig(:pagination, :limit) || 20
+    @records = records.page(page_number).per(per_page) if records.respond_to?(:page)
+  end
+
+  def handle_error(error)
+    # Log the error or send it to an error tracking service
+    Rails.logger.error("PaymentMethodService::Index encountered an error: #{error.message}")
+    # You can define how you want to handle the error, e.g., return an empty array or raise the error
+    raise error
   end
 end
 # rubocop:enable Style/ClassAndModuleChildren
