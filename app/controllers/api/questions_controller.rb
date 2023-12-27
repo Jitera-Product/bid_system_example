b8ac5a7d40f506da8480ebb47426d9244db2bac9
Corissa (Jitera) <developers@jitera.com>
2023-12-27 11:21:29 +0000

    feat[api]: implement 'Create Question' Changes * '/app/controllers/api/questions_controller.rb': + In the `questions_controller.rb` file, you will need to add the `create` action to handle the POST request to `/api/questions`. This action will use the `QuestionSubmissionService` to create a new question with the provided parameters..+ First, ensure th...

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -4,31 +4,45 @@ class Api::QuestionsController < Api::BaseController
   def create
     # Validate contributor role
     unless ContributorValidator.new(contributor_id: question_params[:contributor_id]).valid?
-      render json: { error: 'User is not a contributor' }, status: :forbidden
+      render json: { error: 'Invalid contributor ID or insufficient permissions.' }, status: :forbidden
       return
     end
 
     # Validate presence of title and content
-    if question_params[:title].blank? || question_params[:content].blank?
-      render json: { error: 'Title and content cannot be blank' }, status: :unprocessable_entity
+    if question_params[:title].blank?
+      render json: { error: 'The title is required.' }, status: :unprocessable_entity
+      return
+    elsif question_params[:title].length > 200
+      render json: { error: 'The title cannot exceed 200 characters.' }, status: :unprocessable_entity
+      return
+    end
+
+    if question_params[:content].blank?
+      render json: { error: 'The content is required.' }, status: :unprocessable_entity
       return
     end
 
     # Validate tags
     unless TagValidator.new(tag_ids: question_params[:tags]).valid?
-      render json: { error: 'One or more tags are invalid' }, status: :unprocessable_entity
+      render json: { error: 'One or more tags are invalid.' }, status: :unprocessable_entity
       return
     end
 
     # Create question and associated tags using the service
-    result = QuestionSubmissionService.new(question_params).execute
+    result = QuestionSubmissionService.new(
+      question_params[:contributor_id],
+      question_params[:title],
+      question_params[:content],
+      question_params[:tags]
+    ).call
 
-    if result.success?
-      # Return the id of the newly created question to the contributor.
-      render json: { id: result.question.id, title: result.question.title, content: result.question.content, tags: result.question.tags }, status: :created
+    if result[:success]
+      render json: { status: 201, question: result.except(:success) }, status: :created
     else
-      render json: { errors: result.errors }, status: :unprocessable_entity
+      render json: { error: result[:error] }, status: :unprocessable_entity
     end
+  rescue => e
+    render json: { error: e.message }, status: :internal_server_error
   end
 
   private
