3ee3856cd9e74a1001a2cfeb0d342e54d60a6514
Vevay (Jitera) <developers@jitera.com>
2023-12-27 11:17:06 +0000

    feat[business-logic]: implement 'Edit user profile' Changes * '/app/controllers/api/users_controller.rb': + Update the `update` action to handle the new input fields: "username", "password_hash", and "role". Ensure that the `update_params` method includes these new fields..+ Add a new method `validate_username_uniqueness` that checks if the username is unique ... * '/app/services/user_update_service.rb': + Create a method `update_user_profile` with inputs "user_id" (integer), "username" (string), "password_hash" (string, nullable), "role" (enum_type), and "updated_at" (datetime)..+ Use ActiveRecord's `find_by` method to validate that the `user_id` correspo...

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,15 +1,14 @@
 class Api::UsersController < Api::BaseController
   before_action :doorkeeper_authorize!, only: %i[index create show update]
+  before_action :set_user, only: %i[show update]
+  before_action :validate_username_uniqueness, only: %i[update]
 
   def index
-    # inside service params are checked and whiteisted
     @users = UserService::Index.new(params.permit!, current_resource_owner).execute
     @total_pages = @users.total_pages
   end
 
   def show
-    @user = User.find_by!('users.id = ?', params[:id])
-
     authorize @user, policy_class: Api::UsersPolicy
   end
 
@@ -18,11 +17,12 @@ class Api::UsersController < Api::BaseController
 
     authorize @user, policy_class: Api::UsersPolicy
 
-    return if @user.save
-
-    @error_object = @user.errors.messages
-
-    render status: :unprocessable_entity
+    if @user.save
+      render json: @user, status: :created
+    else
+      @error_object = @user.errors.messages
+      render status: :unprocessable_entity
+    end
   end
 
   def create_params
@@ -30,19 +30,44 @@ class Api::UsersController < Api::BaseController
   end
 
   def update
-    @user = User.find_by('users.id = ?', params[:id])
-    raise ActiveRecord::RecordNotFound if @user.blank?
-
     authorize @user, policy_class: Api::UsersPolicy
 
-    return if @user.update(update_params)
+    if update_params[:password_hash].present?
+      validate_password_strength(update_params[:password_hash])
+      update_params[:password_hash] = @user.password_digest(update_params[:password_hash])
+    end
 
-    @error_object = @user.errors.messages
-
-    render status: :unprocessable_entity
+    if @user.update(update_params.merge(updated_at: Time.current))
+      AuditLogJob.perform_later('User Profile Updated', @user.id)
+      render json: @user, status: :ok
+    else
+      @error_object = @user.errors.messages
+      render status: :unprocessable_entity
+    end
   end
 
   def update_params
-    params.require(:users).permit(:email)
+    params.require(:users).permit(:email, :username, :password_hash, :role)
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by('users.id = ?', params[:id])
+    raise ActiveRecord::RecordNotFound if @user.blank?
+  end
+
+  def validate_username_uniqueness
+    if update_params[:username].present? && @user.username != update_params[:username] && User.exists?(username: update_params[:username])
+      render json: { error: 'Username already taken' }, status: :unprocessable_entity
+      return
+    end
+  end
+
+  def validate_password_strength(password)
+    unless password =~ User::PASSWORD_FORMAT
+      render json: { error: 'Password does not meet requirements' }, status: :unprocessable_entity
+      return
+    end
   end
 end
