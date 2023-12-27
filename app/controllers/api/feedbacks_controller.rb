707387b2fb8c157e8c335f702ff35898854490d3
Briana (Jitera) <developers@jitera.com>
2023-12-27 11:20:15 +0000

    feat[api]: implement 'Submit Feedback' Changes * '/app/controllers/api/feedbacks_controller.rb': + To implement the "Submit Feedback" API, you will need to modify the `feedbacks_controller.rb` file within the `api` namespace..+ The controller should inherit from `Api::BaseController` to ensure it has the necessary authentication and authorization mech...

--- app/controllers/api/feedbacks_controller.rb
+++ app/controllers/api/feedbacks_controller.rb
@@ -4,24 +4,43 @@ class Api::FeedbacksController < Api::BaseController
   def create
     # Ensure the answer exists
     answer = Answer.find_by(id: feedback_params[:answer_id])
-    return render json: { error: 'Answer not found' }, status: :not_found unless answer
+    unless answer
+      return render json: { error: 'Invalid answer ID.' }, status: :bad_request
+    end
 
     # Ensure the inquirer exists and has the correct role
     inquirer = User.find_by(id: feedback_params[:inquirer_id])
     unless inquirer&.role == 'Inquirer'
-      return render json: { error: 'Inquirer not found or not valid' }, status: :forbidden
+      return render json: { error: 'Invalid inquirer ID or insufficient permissions.' }, status: :forbidden
     end
 
-    # Create the feedback
-    feedback = Feedback.new(feedback_params)
-    if feedback.save
-      # Trigger AI model update or answer ranking algorithm update
-      # This is a placeholder for the actual implementation
-      # Example: AiModelUpdateJob.perform_later(feedback.id)
-      
-      render json: { message: 'Feedback submitted successfully' }, status: :created
-    else
-      render json: { errors: feedback.errors.full_messages }, status: :unprocessable_entity
+    # Validate usefulness is a boolean
+    unless [true, false].include?(feedback_params[:usefulness])
+      return render json: { error: 'Usefulness must be true or false.' }, status: :bad_request
+    end
+
+    # Validate comment length
+    if feedback_params[:comment].present? && feedback_params[:comment].length > 500
+      return render json: { error: 'The comment cannot exceed 500 characters.' }, status: :bad_request
+    end
+
+    # Create the feedback using the FeedbackService::Create
+    feedback_service = FeedbackService::Create.new(
+      answer_id: feedback_params[:answer_id],
+      inquirer_id: feedback_params[:inquirer_id],
+      usefulness: feedback_params[:usefulness],
+      comment: feedback_params[:comment]
+    )
+
+    begin
+      feedback = feedback_service.execute
+      render json: { status: 201, feedback: feedback.as_json.merge(created_at: feedback.created_at.iso8601) }, status: :created
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: e.message }, status: :not_found
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+    rescue Exceptions::InquirerNotFoundError => e
+      render json: { error: e.message }, status: :forbidden
     end
   end
 
