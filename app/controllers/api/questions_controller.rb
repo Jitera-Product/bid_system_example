954ba1209b1b9feef1c7150ee506287c34aaa69d
Laraine (Jitera) <developers@jitera.com>
2024-01-11 04:09:14 +0000

    feat[api]: implement 'Submit Question' Changes * '/app/controllers/api/questions_controller.rb': + To implement the "Submit Question" API, you will need to modify the `questions_controller.rb` file within the `api` namespace..+ Add a new action method `submit` to handle the POST request to `/api/questions`..+ Ensure that the `authenticate_user!` befor...

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,10 +1,13 @@
 class Api::QuestionsController < Api::BaseController
   before_action :authenticate_user!
+  before_action :check_contributor_role, only: [:create]
 
   def create
+    validate_question_params
     question_service = QuestionSubmissionService.new(question_params, current_user)
     if question_service.submit
-      render json: { question_id: question_service.question.id }, status: :created
+      question = question_service.question
+      render json: { status: 201, question: question.as_json.merge(contributor_id: current_user.id, created_at: question.created_at) }, status: :created
     else
       render json: { errors: question_service.errors }, status: :unprocessable_entity
     end
@@ -14,5 +17,20 @@ class Api::QuestionsController < Api::BaseController
 
   def question_params
     params.require(:question).permit(:title, :content, :category, tags: [])
+  end
+
+  def check_contributor_role
+    unless current_user.has_role?(:contributor)
+      render json: { error: 'Forbidden' }, status: :forbidden
+    end
+  end
+
+  def validate_question_params
+    errors = {}
+    errors[:title] = "The title is required." if params[:question][:title].blank?
+    errors[:content] = "The content is required." if params[:question][:content].blank?
+    errors[:category] = "The category is required." if params[:question][:category].blank?
+    errors[:tags] = "Tags must be an array." unless params[:question][:tags].is_a?(Array)
+    render json: { errors: errors }, status: :unprocessable_entity if errors.any?
   end
 end
