349e9da2cc07a14cf9db63a2cadb29ba0a96f149
Paolina (Jitera) <developers@jitera.com>
2024-01-11 04:08:31 +0000

    feat[api]: implement 'Retrieve Answer' Changes * '/app/controllers/api/v1/answers_controller.rb': + In the `AnswersController`, create a new action called `search` to handle the GET request to `/api/answers/search`..+ Import the `QuestionMatchingService` from `/app/services/question_matching_service.rb` to use the `match_question` method..+ Use the `pa...

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,6 +1,5 @@
-
 class Api::V1::AnswersController < ApplicationController
-  before_action :authenticate_user!, only: [:retrieve_answer]
+  before_action :authenticate_user!, only: [:retrieve_answer, :search]
 
   # GET /api/v1/answers/retrieve
   def retrieve_answer
@@ -21,6 +20,28 @@ class Api::V1::AnswersController < ApplicationController
       render json: { error: e.message }, status: :unauthorized
     rescue => e
       render json: { error: e.message }, status: :unprocessable_entity
+    end
+  end
+
+  # GET /api/v1/answers/search
+  def search
+    begin
+      query = params[:query]
+      raise ArgumentError, 'The query is required.' if query.blank?
+
+      parsed_query = NlpParserService.parse(query)
+      matching_questions = QuestionMatchingService.find_matching_questions(parsed_query)
+      answers = matching_questions.map do |question|
+        question.answers.select(:id, :content, :question_id)
+      end.flatten
+
+      render json: { status: 200, answers: answers }, status: :ok
+    rescue ArgumentError => e
+      render json: { error: e.message }, status: :bad_request
+    rescue Exceptions::AuthenticationError => e
+      render json: { error: e.message }, status: :unauthorized
+    rescue => e
+      render json: { error: e.message }, status: :internal_server_error
     end
   end
 
