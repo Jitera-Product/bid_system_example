08b19cdc76016a8c49edc27a8b9c64def4c24c05
Trix (Jitera) <developers@jitera.com>
2024-03-08 05:28:45 +0000

    feat[api]: implement Create Todo Item

--- app/controllers/concerns/oauth_tokens_concern.rb
+++ app/controllers/concerns/oauth_tokens_concern.rb
@@ -1,10 +1,21 @@
+
 # frozen_string_literal: true
 
 module OauthTokensConcern
   extend ActiveSupport::Concern
 
   def current_resource_owner
-    doorkeeper_token&.resource_owner
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
   alias current_user current_resource_owner
+
+  included do
+    before_action :validate_oauth_token, only: [:create]
+  end
+
+  private
+
+  def validate_oauth_token
+    raise Exceptions::AuthenticationError unless doorkeeper_token
+  end
 end
